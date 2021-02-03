1ace810f4fa1f2b3f7f4237f3ed43ce0cc173506
Michal Mlejnek <michal.mlejnek@mews.com>
2021-02-03 15:36:28 +0100

    Update icons

--- optimus/lib/src/banner.dart
+++ optimus/lib/src/banner.dart
@@ -138,9 +138,9 @@ class OptimusBanner extends StatelessWidget {
       case OptimusBannerVariant.success:
         return OptimusIcons.done_circle;
       case OptimusBannerVariant.warning:
-        return OptimusIcons.disable; // TODO(MM): add warning icon
+        return OptimusIcons.problematic;
       case OptimusBannerVariant.error:
-        return OptimusIcons.disable; // TODO(MM): update icon
+        return OptimusIcons.blacklist;
     }
   }
 
