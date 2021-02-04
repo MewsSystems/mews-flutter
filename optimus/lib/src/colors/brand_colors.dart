3da415bc780b518dfa438e38e2a240ceb59af838
gladkovskiy <vadim.gladkovskiy@gmail.com>
2021-02-04 11:47:24 +0100

    add accent and secondary colors

---
+++ optimus/lib/src/colors/brand_colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+abstract class OptimusBrandColors {
+  OptimusBrandColors._();
+
+  /// Accent colors
+  ///
+  /// This color needs to be used in either brand-specific variation
+  /// of the system components or sparingly as an accent element to avoid
+  /// conflict with the semantic danger color.
+  static const coral = Color(0xFFFF5E64);
+  static const coralt8 = Color(0x14FF5E64);
+  static const coralt16 = Color(0x28FF5E64);
+  static const coralt24 = Color(0x3DFF5E64);
+
+  /// Secondary colors
+  ///
+  /// The brand secondary colors are the most versatile and can be used for
+  /// brand-specific components, system backgrounds, and other neutral
+  /// elements like cards.
+  static const lightGrey = Color(0xFFEDECEB);
+  static const mediumGrey = Color(0xFFE7E6E4);
+  static const blueGrey = Color(0xFFC5CED6);
+  static const pinkGrey = Color(0xFFD6C9D0);
+}
