5e7633a365274eeaeb33cf0d6eab82ace8011e8a
Kirill Bubochkin <ookami.kb@gmail.com>
2021-02-25 10:38:12 +0100

    :zap: [RND-49612] Add theming support to OptimusAvatar (#28)

--- optimus/lib/src/avatar.dart
+++ optimus/lib/src/avatar.dart
@@ -1,8 +1,7 @@
-import 'dart:math';
-
+import 'package:dfunc/dfunc.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:optimus/optimus.dart';
+import 'package:optimus/src/theme/theme.dart';
 import 'package:optimus/src/typography/styles.dart';
 
 class OptimusAvatar extends StatelessWidget {
@@ -20,47 +19,58 @@ class OptimusAvatar extends StatelessWidget {
   final bool isIndicatorVisible;
 
   @override
-  Widget build(BuildContext context) => Stack(
-        children: <Widget>[
-          _buildCircleAvatar(),
-          if (isIndicatorVisible) _buildIndicator(),
-        ],
-      );
+  Widget build(BuildContext context) {
+    final colors = OptimusTheme.of(context).colors;
 
-  Widget _buildCircleAvatar() => ClipOval(
-        child: CircleAvatar(
-          radius: isSmall ? _smallRadius : _radius,
-          backgroundColor:
-              imageUrl == null ? OptimusLightColors.neutral200 : Colors.white,
-          backgroundImage: imageUrl == null ? null : NetworkImage(imageUrl),
-          child: imageUrl == null
-              ? Text(
-                  _safeSubstring(title, 0, 1).toUpperCase(),
-                  style: preset300s.copyWith(
-                    color: OptimusLightColors.neutral0t64,
-                  ),
-                )
-              : null,
-        ),
-      );
-
-  Widget _buildIndicator() => Positioned(
-        top: -2,
-        left: -2,
-        child: Container(
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: OptimusLightColors.primary500,
-            border: Border.all(width: 2, color: OptimusLightColors.neutral0),
+    return Stack(
+      overflow: Overflow.visible,
+      children: <Widget>[
+        ClipOval(
+          child: CircleAvatar(
+            radius: isSmall ? _smallRadius : _radius,
+            backgroundColor:
+                imageUrl == null ? colors.neutral200 : colors.neutral0,
+            backgroundImage: imageUrl == null ? null : NetworkImage(imageUrl),
+            child: imageUrl == null
+                ? Text(
+                    substring(title, 0, 1).toUpperCase(),
+                    style: preset300s.copyWith(color: colors.neutral0t64),
+                  )
+                : null,
           ),
-          height: 14,
-          width: 14,
         ),
-      );
+        if (isIndicatorVisible) const _Indicator(),
+      ],
+    );
+  }
+}
+
+class _Indicator extends StatelessWidget {
+  const _Indicator({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = OptimusTheme.of(context);
+
+    return Positioned(
+      top: -2,
+      left: -2,
+      child: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: theme.colors.primary500,
+          border: Border.all(
+            width: 2,
+            color:
+                theme.isDark ? theme.colors.neutral500 : theme.colors.neutral0,
+          ),
+        ),
+        height: 14,
+        width: 14,
+      ),
+    );
+  }
 }
 
 const double _smallRadius = 20;
 const double _radius = 24;
-
-String _safeSubstring(String str, int from, int to) =>
-    str == null ? '' : str.substring(from, min(to, str.length));
---
+++ storybook/lib/stories/avatar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/widgets.dart';
+import 'package:optimus/optimus.dart';
+import 'package:storybook_flutter/storybook_flutter.dart';
+
+final avatarStory = Story.simple(
+  name: 'Avatar',
+  child: const OptimusStack(
+    direction: Axis.horizontal,
+    mainAxisSize: MainAxisSize.min,
+    spacing: OptimusStackSpacing.spacing100,
+    children: [
+      OptimusAvatar(title: 'Test'),
+      OptimusAvatar(title: 'Test', isIndicatorVisible: true),
+      OptimusAvatar(title: 'Test', isSmall: false),
+      OptimusAvatar(title: 'Test', isSmall: false, isIndicatorVisible: true),
+    ],
+  ),
+);
