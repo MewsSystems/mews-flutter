bfd444f3490a835f02cc87232043c872e2f71c6b
ookami-kb <ookami.kb@gmail.com>
2021-03-01 12:55:48 +0100

    Update badge

--- optimus/lib/src/badge.dart
+++ optimus/lib/src/badge.dart
@@ -2,57 +2,49 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:optimus/optimus.dart';
 import 'package:optimus/src/theme/theme.dart';
-import 'package:optimus/src/theme/theme_data.dart';
 
 class OptimusBadge extends StatelessWidget {
   const OptimusBadge({
     Key key,
-    this.text,
+    @required this.text,
   }) : super(key: key);
 
   final String text;
 
-  BoxDecoration _decoration(OptimusThemeData theme) => BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(40)),
-        color: theme.colors.primary,
-        border: _border(theme),
-      );
-
-  Border _border(OptimusThemeData theme) => Border.all(
-        width: 2,
-        color: theme.isDark ? theme.colors.neutral500 : theme.colors.neutral0,
-      );
-
   @override
   Widget build(BuildContext context) {
     final theme = OptimusTheme.of(context);
+    final decoration = BoxDecoration(
+      borderRadius: const BorderRadius.all(Radius.circular(10)),
+      color: theme.colors.primary,
+      border: Border.all(
+        width: 2,
+        color: theme.isDark ? theme.colors.neutral500 : theme.colors.neutral0,
+      ),
+    );
 
     return Container(
       constraints: const BoxConstraints(minWidth: 20),
       height: 20,
-      decoration: _decoration(theme),
-      child: Padding(
-        padding: _padding,
-        child: IntrinsicWidth(
-          child: Center(
-            child: OptimusLabel(
-              variation: Variation.variationSecondary,
-              child: Text(
-                text,
-                style: TextStyle(
-                  color: theme.isDark
-                      ? theme.colors.neutral1000
-                      : theme.colors.neutral0,
-                  height: 1,
-                ),
-              ),
-            ),
+      decoration: decoration,
+      padding: const EdgeInsets.symmetric(
+        horizontal: spacing50,
+        vertical: spacing25,
+      ),
+      child: IntrinsicWidth(
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          // TODO(KB): Sync with text presets
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w600,
+            color:
+                theme.isDark ? theme.colors.neutral1000 : theme.colors.neutral0,
+            height: 1,
           ),
         ),
       ),
     );
   }
-
-  EdgeInsets get _padding =>
-      const EdgeInsets.symmetric(horizontal: spacing50, vertical: spacing25);
 }
