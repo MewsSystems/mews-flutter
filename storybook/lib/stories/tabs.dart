a48f55a82f3d513ebd1843dae8d16fddbea9907e
ookami-kb <ookami.kb@gmail.com>
2021-03-03 15:42:53 +0100

    Update story

--- storybook/lib/stories/tabs.dart
+++ storybook/lib/stories/tabs.dart
@@ -9,13 +9,13 @@ final Story tabs = Story.simple(
     color: Colors.white,
     constraints: const BoxConstraints(maxWidth: 400, maxHeight: 200),
     child: OptimusTabBar(
-      tabs: _tabs(),
+      tabs: _tabs,
       pages: _pages,
     ),
   ),
 );
 
-List<Widget> _tabs() =>
+List<Widget> get _tabs =>
     _items.map((i) => OptimusTab(text: 'Tab ${i + 1}')).toList();
 
 List<Widget> get _pages =>
