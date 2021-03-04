e5d51c7fd1241033f9adb7099da6e872f5121adf
Vadim Gladkovskiy <vadim.gladkovskiy@gmail.com>
2021-03-04 12:33:54 +0100

    :zap: [RND-49612] Add dark theme support for iconlist tileborders and expansion tile (#45)

--- optimus/lib/src/borders.dart
+++ optimus/lib/src/borders.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:optimus/src/colors/colors.dart';
-
-const Color _borderColor = OptimusLightColors.neutral50;
+import 'package:optimus/src/theme/theme.dart';
 
 @Deprecated('Use higher level components instead.')
 class TileBorders extends StatelessWidget {
@@ -11,26 +9,30 @@ class TileBorders extends StatelessWidget {
   final Widget child;
 
   @override
-  Widget build(BuildContext context) => Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Stack(
-            overflow: Overflow.visible,
-            children: <Widget>[
-              Positioned(
-                bottom: -1,
-                left: -1,
-                right: -1,
-                top: 0,
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: _borderColor),
-                  ),
+  Widget build(BuildContext context) {
+    final theme = OptimusTheme.of(context);
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Stack(
+          overflow: Overflow.visible,
+          children: <Widget>[
+            Positioned(
+              bottom: -1,
+              left: -1,
+              right: -1,
+              top: 0,
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: theme.borderColor),
                 ),
               ),
-              child,
-            ],
-          ),
-        ],
-      );
+            ),
+            child,
+          ],
+        ),
+      ],
+    );
+  }
 }
