aa9a12c36a2bb794de45ffaf80e66c7634177936
gladkovskiy <vadim.gladkovskiy@gmail.com>
2021-05-11 12:36:36 +0200

    fix analyzer error

--- kiosk_mode/lib/kiosk_mode.dart
+++ kiosk_mode/lib/kiosk_mode.dart
@@ -4,10 +4,7 @@ import 'package:flutter/services.dart';
 
 const MethodChannel _channel = MethodChannel('kiosk_mode');
 
-Future<void> get startKioskMode async {
-  return _channel.invokeMethod('startKioskMode');
-}
+Future<void> get startKioskMode async =>
+    _channel.invokeMethod('startKioskMode');
 
-Future<void> get stopKioskMode async {
-  return _channel.invokeMethod('stopKioskMode');
-}
+Future<void> get stopKioskMode async => _channel.invokeMethod('stopKioskMode');
