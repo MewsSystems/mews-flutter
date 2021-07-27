e4eb3b84cf7225bd2e6a8ac63c41411e535fc7e9
ookami-kb <ookami.kb@gmail.com>
2021-07-27 10:48:14 +0200

    Code style

--- optimus/lib/src/elevation.dart
+++ optimus/lib/src/elevation.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:optimus/optimus.dart';
 
 const List<BoxShadow> elevation0 = [];
 
 const List<BoxShadow> elevation25 = [
   BoxShadow(
-    color: Color(0x14000000),
+    color: _blackt8,
     offset: Offset(0, 4),
     blurRadius: 8,
   ),
   BoxShadow(
-    color: Color(0x14000000),
+    color: _blackt8,
     offset: Offset(0, -1),
     blurRadius: 4,
   ),
@@ -19,12 +18,12 @@ const List<BoxShadow> elevation25 = [
 
 const List<BoxShadow> elevation50 = [
   BoxShadow(
-    color: Color(0x14000000),
+    color: _blackt8,
     offset: Offset(0, 8),
     blurRadius: 16,
   ),
   BoxShadow(
-    color: Color(0x14000000),
+    color: _blackt8,
     offset: Offset(0, -1),
     blurRadius: 4,
   ),
@@ -32,13 +31,16 @@ const List<BoxShadow> elevation50 = [
 
 const List<BoxShadow> elevation100 = [
   BoxShadow(
-    color: Color(0x28000000),
+    color: _blackt16,
     offset: Offset(0, 8),
     blurRadius: 24,
   ),
   BoxShadow(
-    color: Color(0x14000000),
+    color: _blackt8,
     offset: Offset(0, -1),
     blurRadius: 4,
   ),
 ];
+
+const _blackt8 = Color(0x14000000);
+const _blackt16 = Color(0x28000000);
