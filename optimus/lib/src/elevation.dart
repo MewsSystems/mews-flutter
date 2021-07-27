43cb1c250b5633553a2fcb752f5f6e959fd37f11
ookami-kb <ookami.kb@gmail.com>
2021-07-27 10:43:37 +0200

    fix(optimus): Fix shadow colors

--- optimus/lib/src/elevation.dart
+++ optimus/lib/src/elevation.dart
@@ -6,12 +6,12 @@ const List<BoxShadow> elevation0 = [];
 
 const List<BoxShadow> elevation25 = [
   BoxShadow(
-    color: OptimusLightColors.neutral500t8,
+    color: Color(0x14000000),
     offset: Offset(0, 4),
     blurRadius: 8,
   ),
   BoxShadow(
-    color: OptimusLightColors.neutral500t8,
+    color: Color(0x14000000),
     offset: Offset(0, -1),
     blurRadius: 4,
   ),
@@ -19,12 +19,12 @@ const List<BoxShadow> elevation25 = [
 
 const List<BoxShadow> elevation50 = [
   BoxShadow(
-    color: OptimusLightColors.neutral500t8,
+    color: Color(0x14000000),
     offset: Offset(0, 8),
     blurRadius: 16,
   ),
   BoxShadow(
-    color: OptimusLightColors.neutral500t8,
+    color: Color(0x14000000),
     offset: Offset(0, -1),
     blurRadius: 4,
   ),
@@ -32,12 +32,12 @@ const List<BoxShadow> elevation50 = [
 
 const List<BoxShadow> elevation100 = [
   BoxShadow(
-    color: OptimusLightColors.neutral500t16,
+    color: Color(0x28000000),
     offset: Offset(0, 8),
     blurRadius: 24,
   ),
   BoxShadow(
-    color: OptimusLightColors.neutral500t8,
+    color: Color(0x14000000),
     offset: Offset(0, -1),
     blurRadius: 4,
   ),
