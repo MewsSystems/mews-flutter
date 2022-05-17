87f687d98114c6cb691a12fb3d668d9a6cba1df0
Vitalij Vascenko <vetal.zt@gmail.com>
2022-05-17 10:31:40 +0200

    Replaced with stateless widget. Code refactoring

--- optimus/lib/src/notification.dart
+++ optimus/lib/src/notification.dart
@@ -5,6 +5,14 @@ import 'package:optimus/src/border_radius.dart';
 import 'package:optimus/src/elevation.dart';
 import 'package:optimus/src/typography/presets.dart';
 
+const double _kMaxWidth = 360;
+const double _kCloseIconSize = 16;
+const double _kIconSize = 20;
+const double _kIconHorizontalPadding = 10;
+const int _kMaxLinesTitle = 1;
+const int _kMaxLinesDescription = 5;
+const int _kMaxLinesLink = 1;
+
 /// Describes certain type of notification with it's semantical meaning.
 /// Use-cases:
 ///  - [OptimusNotificationVariant.info] -  Used for notifying about
@@ -64,7 +72,7 @@ extension on OptimusNotificationVariant {
 /// Notification are used for showing brief and concise message that
 /// communicates immediate feedback with optional action included. Notifications
 /// are noticeable but not intrusive to the use and can be temporary.
-class OptimusNotification extends StatefulWidget {
+class OptimusNotification extends StatelessWidget {
   const OptimusNotification({
     Key? key,
     required this.title,
@@ -86,112 +94,133 @@ class OptimusNotification extends StatefulWidget {
   final VoidCallback? onDismiss;
   final OptimusNotificationVariant variant;
 
-  @override
-  State<OptimusNotification> createState() => _OptimusNotificationState();
-}
-
-class _OptimusNotificationState extends State<OptimusNotification>
-    with ThemeGetter {
-  Widget get _closeIcon => GestureDetector(
-        onTap: () => widget.onDismiss,
+  Widget _closeIcon(OptimusThemeData theme) => GestureDetector(
+        onTap: () => onDismiss?.call(),
         child: Padding(
-          padding: const EdgeInsets.all(6),
+          padding: const EdgeInsets.all(spacing100),
           child: Icon(
             OptimusIcons.cross_close,
             color: theme.colors.neutral500,
-            size: 16,
+            size: _kCloseIconSize,
           ),
         ),
       );
 
   @override
   Widget build(BuildContext context) {
-    final double notificationWidth =
-        min(MediaQuery.of(context).size.width - spacing200, 360);
+    final OptimusThemeData theme = OptimusTheme.of(context);
+    final Breakpoint screenBreakpoint = MediaQuery.of(context).screenBreakpoint;
+    final padding = screenBreakpoint.getPadding();
+    final double notificationWidth = min(
+      MediaQuery.of(context).size.width - padding * 2,
+      _kMaxWidth,
+    );
 
-    return Container(
-      constraints: BoxConstraints(maxWidth: notificationWidth),
-      decoration: BoxDecoration(
-        color: widget.variant.getBannerColor(theme),
-        borderRadius: const BorderRadius.all(borderRadius50),
-        boxShadow: elevation50,
-      ),
-      child: Stack(
-        children: [
-          Row(
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: spacing100),
-                child: Icon(
-                  widget.icon ?? widget.variant.getBannerIcon(),
-                  color: widget.variant.getBannerIconColor(theme),
-                  size: 24,
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.all(spacing200),
-                  decoration: BoxDecoration(
-                    color: theme.colors.neutral0,
-                    borderRadius: const BorderRadius.horizontal(
-                      right: borderRadius50,
-                    ),
+    return Positioned(
+      top: padding,
+      right: padding,
+      child: Container(
+        constraints: BoxConstraints(maxWidth: notificationWidth),
+        decoration: BoxDecoration(
+          color: variant.getBannerColor(theme),
+          borderRadius: const BorderRadius.all(borderRadius50),
+          boxShadow: elevation50,
+        ),
+        child: Stack(
+          children: [
+            Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: _kIconHorizontalPadding),
+                  child: Icon(
+                    icon ?? variant.getBannerIcon(),
+                    color: variant.getBannerIconColor(theme),
+                    size: _kIconSize,
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: spacing100,
-                          bottom: spacing50,
-                        ),
-                        child: Text(
-                          widget.title,
-                          maxLines: 1,
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(spacing200),
+                    decoration: BoxDecoration(
+                      color: theme.colors.neutral0,
+                      borderRadius: const BorderRadius.horizontal(
+                        right: borderRadius50,
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          title,
+                          maxLines: _kMaxLinesTitle,
                           overflow: TextOverflow.ellipsis,
                           style: preset300b.copyWith(
                             color: theme.colors.neutral1000,
                           ),
                         ),
-                      ),
-                      if (widget.description != null)
-                        Padding(
-                          padding:
-                              const EdgeInsets.symmetric(vertical: spacing50),
-                          child: Text(
-                            widget.description!,
-                            maxLines: 5,
-                            overflow: TextOverflow.ellipsis,
-                            style: preset200r.copyWith(
-                              color: theme.colors.neutral1000t64,
+                        if (description != null)
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              top: spacing50,
+                            ),
+                            child: Text(
+                              description!,
+                              maxLines: _kMaxLinesDescription,
+                              overflow: TextOverflow.ellipsis,
+                              style: preset200r.copyWith(
+                                color: theme.colors.neutral1000t64,
+                              ),
                             ),
                           ),
-                        ),
-                      if (widget.link != null)
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: spacing50,
-                          ),
-                          child: Text(
-                            widget.link!,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: preset200b.copyWith(
-                              decoration: TextDecoration.underline,
+                        if (link != null)
+                          GestureDetector(
+                            onTap: () => onLinkPressed?.call(),
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                top: spacing50,
+                              ),
+                              child: Text(
+                                link!,
+                                maxLines: _kMaxLinesLink,
+                                overflow: TextOverflow.ellipsis,
+                                style: preset200b.copyWith(
+                                  color: theme.colors.neutral1000,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
                             ),
-                          ),
-                        )
-                    ],
+                          )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          if (widget.isDismissible)
-            Positioned(right: spacing100, top: spacing100, child: _closeIcon)
-        ],
+              ],
+            ),
+            if (isDismissible)
+              Positioned(
+                right: spacing100,
+                top: spacing100,
+                child: _closeIcon(theme),
+              )
+          ],
+        ),
       ),
     );
+  }
+}
+
+extension on Breakpoint {
+  double getPadding() {
+    switch (this) {
+      case Breakpoint.medium:
+      case Breakpoint.large:
+      case Breakpoint.extraLarge:
+        return spacing200;
+      case Breakpoint.small:
+      case Breakpoint.extraSmall:
+        return spacing100;
+    }
   }
 }
