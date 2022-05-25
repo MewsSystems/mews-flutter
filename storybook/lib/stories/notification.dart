5f42798fcef551a68ddbd0c557fff82b4a61e160
Vitalij Vaščenko <vetal.zt@gmail.com>
2022-05-25 16:45:01 +0200

    feat(optimus): [RND-103047] Add Notification component  (#209)

---
+++ storybook/lib/stories/notification.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:optimus/optimus.dart';
+import 'package:storybook_flutter/storybook_flutter.dart';
+
+final Story notificationStory = Story(
+  name: 'Notification',
+  builder: (context) {
+    final k = context.knobs;
+    final title = k.text(label: 'Title', initial: 'Title');
+    final body = k.text(label: 'Notification body', initial: '');
+    final link = k.text(label: 'Link text', initial: '');
+    final dismissible = k.boolean(label: 'Is Dismissible');
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: OptimusNotificationVariant.values
+            .map(
+              (variant) => OptimusNotification(
+                title: title,
+                body: body.isNotEmpty ? body : null,
+                variant: variant,
+                link: link.isNotEmpty ? link : null,
+                onDismissed: dismissible ? () {} : null,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  },
+);
