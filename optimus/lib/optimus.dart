a69109ffbe56ca75f932cc54a91cbd3ac075f6f4
Michal Mlejnek <chaemil72@gmail.com>
2022-06-09 19:36:38 +0200

    fix(optimus): Dark theme text color (#218)

--- optimus/lib/optimus.dart
+++ optimus/lib/optimus.dart
@@ -73,6 +73,7 @@ export 'src/typography/variation.dart';
 export 'src/widget_size.dart';
 
 ThemeData createOptimusMaterialTheme(Brightness brightness) => ThemeData(
+      brightness: brightness,
       scaffoldBackgroundColor: OptimusColors(brightness).background,
       primarySwatch: Colors.blue,
       fontFamily: 'packages/optimus/OpenSans',
@@ -91,11 +92,11 @@ ThemeData createOptimusMaterialTheme(Brightness brightness) => ThemeData(
         button: baseTextStyle,
         overline: baseTextStyle,
       ),
-      cupertinoOverrideTheme: const CupertinoThemeData.raw(
-        Brightness.light,
+      cupertinoOverrideTheme: CupertinoThemeData.raw(
+        brightness,
         Colors.blue,
         Colors.blue,
-        CupertinoTextThemeData(),
+        const CupertinoTextThemeData(),
         Colors.white,
         Colors.white,
       ),
