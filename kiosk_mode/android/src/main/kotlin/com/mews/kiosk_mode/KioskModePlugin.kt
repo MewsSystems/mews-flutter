ccfdcf6274c2c765c79aea5c6591041043ff5ca4
Vlad Alboiu <45460964+alboiuvlad29@users.noreply.github.com>
2022-06-14 09:33:05 +0100

    feat(kiosk_mode): [RND-106334] move watchKioskMode implementation on platform side (android) (#210)

--- kiosk_mode/android/src/main/kotlin/com/mews/kiosk_mode/KioskModePlugin.kt
+++ kiosk_mode/android/src/main/kotlin/com/mews/kiosk_mode/KioskModePlugin.kt
@@ -4,71 +4,89 @@ import android.app.Activity
 import android.app.ActivityManager
 import android.content.Context
 import android.view.ViewGroup
-import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
+
+private const val methodChannelName = "com.mews.kiosk_mode/kiosk_mode"
+private const val eventChannelName = "com.mews.kiosk_mode/kiosk_mode_stream"
 
 class KioskModePlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     private lateinit var channel: MethodChannel
+    private lateinit var eventChannel: EventChannel
     private var activity: Activity? = null
 
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "com.mews.kiosk_mode/kiosk_mode")
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, methodChannelName)
         channel.setMethodCallHandler(this)
+
+        eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, eventChannelName)
+        eventChannel.setStreamHandler(KioskModeStreamHandler(this::isInKioskMode))
     }
 
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-        if (call.method == "startKioskMode") {
-            activity?.let { a ->
-                // ensures that startLockTask() will not throw
-                // see https://stackoverflow.com/questions/27826431/activity-startlocktask-occasionally-throws-illegalargumentexception
-                a.findViewById<ViewGroup>(android.R.id.content).getChildAt(0).post {
-                    try {
-                        a.startLockTask()
-                        result.success(true)
-                    } catch (e: IllegalArgumentException) {
-                        result.success(false)
-                    }
-                }
-            } ?: result.success(false)
-        } else if (call.method == "stopKioskMode") {
-            activity?.stopLockTask()
-            result.success(null)
-        } else if (call.method == "isInKioskMode") {
-            val service = activity?.getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager
-            if (service == null) {
-                result.success(null)
-                return
-            }
-
-            val isInKioskMode = when (service.lockTaskModeState) {
-                ActivityManager.LOCK_TASK_MODE_NONE -> false
-                ActivityManager.LOCK_TASK_MODE_PINNED,
-                ActivityManager.LOCK_TASK_MODE_LOCKED -> true
-                else -> false
-            }
-
-            result.success(isInKioskMode)
-        } else if (call.method == "isManagedKiosk") {
-            val service = activity?.getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager
-            if (service == null) {
-                result.success(null)
-                return
-            }
-
-            result.success(service.lockTaskModeState == ActivityManager.LOCK_TASK_MODE_LOCKED)
-        } else {
-            result.notImplemented()
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        when (call.method) {
+            "startKioskMode" -> startKioskMode(result)
+            "stopKioskMode" -> stopKioskMode(result)
+            "isInKioskMode" -> isInKioskMode(result)
+            "isManagedKiosk" -> isManagedKiosk(result)
+            else -> result.notImplemented()
         }
     }
 
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    private fun startKioskMode(result: MethodChannel.Result) {
+        activity?.let { a ->
+            // ensures that startLockTask() will not throw
+            // see https://stackoverflow.com/questions/27826431/activity-startlocktask-occasionally-throws-illegalargumentexception
+            a.findViewById<ViewGroup>(android.R.id.content).getChildAt(0).post {
+                try {
+                    a.startLockTask()
+                    result.success(true)
+                } catch (e: IllegalArgumentException) {
+                    result.success(false)
+                }
+            }
+        } ?: result.success(false)
+    }
+
+    private fun stopKioskMode(result: MethodChannel.Result) {
+        activity?.stopLockTask()
+        result.success(null)
+    }
+
+
+    private fun isManagedKiosk(result: MethodChannel.Result) {
+        val service = activity?.getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager
+        if (service == null) {
+            result.success(null)
+            return
+        }
+
+        result.success(service.lockTaskModeState == ActivityManager.LOCK_TASK_MODE_LOCKED)
+    }
+
+    private fun isInKioskMode(result: MethodChannel.Result) {
+        result.success(isInKioskMode())
+    }
+
+    private fun isInKioskMode(): Boolean? {
+        val service = activity?.getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager
+            ?: return null
+
+        return when (service.lockTaskModeState) {
+            ActivityManager.LOCK_TASK_MODE_PINNED,
+            ActivityManager.LOCK_TASK_MODE_LOCKED -> true
+            else -> false
+        }
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
+        eventChannel.setStreamHandler(null)
     }
 
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
