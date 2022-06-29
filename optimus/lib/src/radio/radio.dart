f7254ee23ef73b3a41ca17e6fcb0f99ad8c9f426
Vitalij Vascenko <vetal.zt@gmail.com>
2022-06-29 15:21:43 +0200

    Replaced IntrinsicHeight.

--- optimus/lib/src/radio/radio.dart
+++ optimus/lib/src/radio/radio.dart
@@ -119,28 +119,41 @@ class _OptimusRadioState<T> extends State<OptimusRadio<T>> with ThemeGetter {
               onTapDown: (_) => setState(() => _isTappedDown = true),
               onTapUp: (_) => setState(() => _isTappedDown = false),
               onTapCancel: () => setState(() => _isTappedDown = false),
-              child: IntrinsicHeight(
-                child: Row(
-                  children: [
-                    Align(
-                      alignment: Alignment.topCenter,
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: Container(
+                      alignment: Alignment.topLeft,
+                      width: _leadingSize,
                       child: _RadioCircle(
                         isSelected: _isSelected,
                         isActive: _isHovering || _isTappedDown,
                       ),
                     ),
-                    Expanded(
-                      child: Padding(
-                        padding:
-                            const EdgeInsets.symmetric(vertical: spacing25),
-                        child: DefaultTextStyle.merge(
-                          style: _labelStyle,
-                          child: widget.label,
-                        ),
-                      ),
+                  ),
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      minHeight: _leadingSize,
                     ),
-                  ],
-                ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const SizedBox(width: _leadingSize),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: spacing25,
+                            ),
+                            child: DefaultTextStyle.merge(
+                              style: _labelStyle,
+                              child: widget.label,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
           ),
@@ -187,3 +200,5 @@ class _RadioCircle extends StatelessWidget {
     );
   }
 }
+
+const double _leadingSize = 32;
