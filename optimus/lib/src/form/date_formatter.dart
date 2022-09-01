950adc19b44052d2498cb87f9e6ad5e6be223b73
Vitalij Vascenko <vetal.zt@gmail.com>
2022-09-01 13:31:59 +0200

    Added docs.

--- optimus/lib/src/form/date_formatter.dart
+++ optimus/lib/src/form/date_formatter.dart
@@ -3,64 +3,49 @@ import 'package:flutter/services.dart';
 
 /// TextInputFormatter that will format the input according to the given mask.
 ///
-/// Mask could consist of the following characters:
-///
-/// - `#`: Digit
-///
-/// Example: `###-###-###`
-///
-/// The [_userInput] is used for tracking the user input and distinct it from
-/// the placeholder, which is used to fill empty places.
+/// DateFormatter will force the input to follow the mask and will fill the
+/// empty spaces with the placeholder.
 class DateFormatter extends TextInputFormatter {
   DateFormatter({
     required this.mask,
     required this.placeholder,
     required List<int> userInput,
-    required this.onUserInputChanged,
-    required this.allowedDigits,
+    this.onUserInputChanged,
+    this.allowedDigits,
   }) {
     _userInput = userInput;
     _buildMaskMap();
   }
 
+  /// The mask to format the input.
+  ///
+  /// # - is a place designed for a digit. Any
+  /// other character will be displayed as is.
+  ///
+  /// Example: '##-##-####'
   final String mask;
+
+  /// The placeholder to fill the empty spaces in the mask.
+  ///
+  /// Example: 'DD-MM-YYYY'
   final String placeholder;
-  final RegExp allowedDigits;
+
+  /// Digits that are allowed to be entered by the user.
+  final RegExp? allowedDigits;
+
+  /// Map of indexes and the corresponding mask characters.
   final Map<int, String> _mask = {};
 
-  // Callback for updating the "clear all" button state.
-  final VoidCallback onUserInputChanged;
+  // Callback, when the user input changes.
+  final VoidCallback? onUserInputChanged;
 
+  /// List of indexes of the user input.
+  ///
+  /// Because the field is then filled with the placeholder, we need to keep
+  /// track of the user input.
   late List<int> _userInput;
 
-  bool _isValidForPosition(String value, int position) {
-    if (position >= mask.length) return false;
-
-    if (mask[position] == _digitSymbol) {
-      return _isValidDigit(value);
-    } else {
-      return false;
-    }
-  }
-
-  bool _isValidDigit(String value) => allowedDigits.hasMatch(value);
-
-  int get _maxLength => mask.length;
-
-  bool get _isComplete => _validInputLength == _getCleanMask.length;
-
-  int get _validInputLength =>
-      _userInput.where((i) => !_isDesignatedSpace(i)).length;
-
-  String get _getCleanMask {
-    String result = placeholder;
-    for (final char in _mask.values) {
-      result = result.replaceAll(char, '');
-    }
-
-    return result;
-  }
-
+  /// Builds the map of indexes and the corresponding mask characters.
   void _buildMaskMap() {
     final StringBuffer buffer = StringBuffer();
     int sectionStart = 0;
@@ -87,6 +72,40 @@ class DateFormatter extends TextInputFormatter {
     }
   }
 
+  int get _maxLength => mask.length;
+
+  bool get _isComplete => _validInputLength == _getCleanMask.length;
+
+  RegExp get _digitsRegExp => allowedDigits ?? RegExp(r'[0-9]');
+
+  /// Returns the list of user input indexes that are not mask positions.
+  int get _validInputLength =>
+      _userInput.where((i) => !_isDesignatedSpace(i)).length;
+
+  /// Return the placeholder without the mask characters.
+  String get _getCleanMask {
+    String result = placeholder;
+    for (final char in _mask.values) {
+      result = result.replaceAll(char, '');
+    }
+
+    return result;
+  }
+
+  /// Returns weather the given value is allowed to be at the given index.
+  bool _isValidForPosition(String value, int position) {
+    if (position >= mask.length) return false;
+
+    if (mask[position] == _digitSymbol) {
+      return _isValidDigit(value);
+    } else {
+      return false;
+    }
+  }
+
+  bool _isValidDigit(String value) => _digitsRegExp.hasMatch(value);
+
+  /// Returns true if the given index is a space designated by the mask.
   bool _isDesignatedSpace(int index) {
     for (final key in _mask.keys) {
       final length = _mask[key]?.length ?? 0;
@@ -98,6 +117,7 @@ class DateFormatter extends TextInputFormatter {
     return false;
   }
 
+  /// Returns the next index that should be filled by the user.
   int _getNextInputIndex(int index, int max) {
     while (_isDesignatedSpace(index)) {
       if (index == max) return max;
@@ -107,6 +127,7 @@ class DateFormatter extends TextInputFormatter {
     return index;
   }
 
+  /// Returns the previous index that should be filled by the user.
   int _getPreviousInputIndex(int index) {
     while (_isDesignatedSpace(index)) {
       if (index == 0) return -1;
@@ -116,23 +137,15 @@ class DateFormatter extends TextInputFormatter {
     return index;
   }
 
-  void _updateMaskBefore(int index, void Function(int) operation) {
+  /// Returns the list of indexes designated for the mask before the given
+  /// index.
+  List<int> _getMaskPositionsBeforeIndex(int index) {
     final int prevValue = _getPreviousInputIndex(index);
     final int start = prevValue + 1;
     final int length = _mask[start]?.length ?? -1;
-    if (length == -1) return;
+    if (length == -1) return [];
 
-    for (int i = start; i < start + length; i++) {
-      operation(i);
-    }
-  }
-
-  void _addMaskBefore(int index) {
-    _updateMaskBefore(index, (i) => _userInput.add(i));
-  }
-
-  void _removeMaskBefore(int index) {
-    _updateMaskBefore(index, (i) => _userInput.remove(i));
+    return List<int>.generate(length, (i) => start + i);
   }
 
   String _replaceCharAt(String value, int index, String replacement) =>
@@ -142,6 +155,17 @@ class DateFormatter extends TextInputFormatter {
           ? ''
           : value.substring(index + replacement.length));
 
+  /// Because the input is filled with the placeholder, the whole process is
+  /// going to be something like this:
+  /// oldValue = 'DD-MM-YYYY'
+  /// newValue = '2DD-MM-YYYY'
+  ///              ^ - will be removed
+  /// result = '2D-MM-YYYY'
+  /// or:
+  /// oldValue = '23-05-2019'
+  /// newValue = '23-05-201' <- we removed the last digit
+  /// empty space will be filled with the placeholder
+  /// result = '23-05-201Y'
   @override
   TextEditingValue formatEditUpdate(
     TextEditingValue oldValue,
@@ -163,7 +187,7 @@ class DateFormatter extends TextInputFormatter {
       if (_isValidForPosition(newText[0], selectPosition)) {
         _userInput.add(selectPosition);
         if (selectPosition - 1 >= 0 && _isDesignatedSpace(selectPosition - 1)) {
-          _addMaskBefore(selectPosition - 1);
+          _userInput.addAll(_getMaskPositionsBeforeIndex(selectPosition - 1));
         }
         resultText = _replaceCharAt(placeholder, selectPosition, newText[0]);
         resultSelection = TextSelection.collapsed(
@@ -192,9 +216,10 @@ class DateFormatter extends TextInputFormatter {
                 nextInputSpace,
               ) &&
               !_userInput.contains(nextInputSpace)) {
-            if (_userInput.isNotEmpty) onUserInputChanged.call();
-            _addMaskBefore(nextInputSpace - 1);
-            _userInput.add(nextInputSpace);
+            if (_userInput.isNotEmpty) onUserInputChanged?.call();
+            _userInput
+              ..addAll(_getMaskPositionsBeforeIndex(nextInputSpace - 1))
+              ..add(nextInputSpace);
             resultText = _replaceCharAt(
               oldText,
               nextInputSpace,
@@ -211,7 +236,8 @@ class DateFormatter extends TextInputFormatter {
         } else {
           _userInput.add(oldSelectionStart);
           if (_isDesignatedSpace(oldSelectionStart - 1)) {
-            _addMaskBefore(oldSelectionStart - 1);
+            _userInput
+                .addAll(_getMaskPositionsBeforeIndex(oldSelectionStart - 1));
           }
 
           resultText = _replaceCharAt(
@@ -233,11 +259,12 @@ class DateFormatter extends TextInputFormatter {
               : oldSelection.end;
 
           _userInput.removeWhere((value) => value >= start && value < end);
-          if (_userInput.isEmpty) onUserInputChanged.call();
+          if (_userInput.isEmpty) onUserInputChanged?.call();
           int selectionPosition = _getPreviousInputIndex(start);
 
           if (start - 1 >= 0 && _isDesignatedSpace(start - 1)) {
-            _removeMaskBefore(start - 1);
+            _userInput
+                .removeWhere(_getMaskPositionsBeforeIndex(start - 1).contains);
             selectionPosition = _getPreviousInputIndex(start - 1) + 1;
           }
 
@@ -270,7 +297,7 @@ class DateFormatter extends TextInputFormatter {
         resultSelection = newSelection;
       }
     }
-    if (oldInputLength != _userInput.length) onUserInputChanged.call();
+    if (oldInputLength != _userInput.length) onUserInputChanged?.call();
 
     return TextEditingValue(
       text: resultText,
