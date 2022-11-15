03adc14e8ad34c42a45240219707a559ff4ede0a
Vitalij Vascenko <vetal.zt@gmail.com>
2022-11-15 10:24:44 +0100

    Update elevation.

--- optimus/lib/src/elevation.dart
+++ optimus/lib/src/elevation.dart
@@ -5,42 +5,49 @@ const List<BoxShadow> elevation0 = [];
 
 const List<BoxShadow> elevation25 = [
   BoxShadow(
-    color: _blackt8,
+    color: _shadowt8,
     offset: Offset(0, 4),
-    blurRadius: 8,
+    blurRadius: 4,
+    spreadRadius: -2,
   ),
   BoxShadow(
-    color: _blackt8,
+    color: _shadowt16,
     offset: Offset(0, -1),
     blurRadius: 4,
+    spreadRadius: -2,
   ),
 ];
 
 const List<BoxShadow> elevation50 = [
   BoxShadow(
-    color: _blackt8,
+    color: _shadowt10,
     offset: Offset(0, 8),
-    blurRadius: 16,
+    blurRadius: 24,
+    spreadRadius: -4,
   ),
   BoxShadow(
-    color: _blackt8,
-    offset: Offset(0, -1),
-    blurRadius: 4,
+    color: _shadowt10,
+    offset: Offset(0, 6),
+    blurRadius: 12,
+    spreadRadius: -6,
   ),
 ];
 
 const List<BoxShadow> elevation100 = [
   BoxShadow(
-    color: _blackt16,
-    offset: Offset(0, 8),
-    blurRadius: 24,
+    color: _shadowt10,
+    offset: Offset(0, 18),
+    blurRadius: 88,
+    spreadRadius: -4,
   ),
   BoxShadow(
-    color: _blackt8,
-    offset: Offset(0, -1),
-    blurRadius: 4,
+    color: _shadowt10,
+    offset: Offset(0, 12),
+    blurRadius: 28,
+    spreadRadius: -6,
   ),
 ];
 
-const _blackt8 = Color(0x14000000);
-const _blackt16 = Color(0x28000000);
+const _shadowt8 = Color(0x14242435);
+const _shadowt10 = Color(0x1a242435);
+const _shadowt16 = Color(0x29242435);
