f7be18d1a9e35bbb19e9255383b89f465225f680
Vitalij Vascenko <vetal.zt@gmail.com>
2023-03-19 21:09:20 +0100

    feat(optimus): [RND-164809] Updated Segmented Control component

--- optimus/lib/src/segmented_control/segmented_control.dart
+++ optimus/lib/src/segmented_control/segmented_control.dart
@@ -5,6 +5,8 @@ import 'package:optimus/src/common/group_wrapper.dart';
 import 'package:optimus/src/segmented_control/wrapper.dart';
 import 'package:optimus/src/typography/presets.dart';
 
+/// Segmented Control is a set of two or more segments, that provide closely
+/// related choices that affect an object, state, or view.
 class OptimusSegmentedControl<T> extends StatelessWidget {
   OptimusSegmentedControl({
     Key? key,
@@ -16,6 +18,7 @@ class OptimusSegmentedControl<T> extends StatelessWidget {
     this.error,
     this.isEnabled = true,
     this.isRequired = false,
+    this.direction = Axis.horizontal,
   })  : assert(
           items.map((i) => i.value).contains(value),
           'Segmented control should always have some existing value',
@@ -23,24 +26,72 @@ class OptimusSegmentedControl<T> extends StatelessWidget {
         items = List.unmodifiable(items),
         super(key: key);
 
+  /// Size of the segmented control.
   final OptimusWidgetSize size;
 
+  /// List of segment control items.
   final List<OptimusGroupItem<T>> items;
 
+  /// Current selected value.
   final T value;
 
+  /// Callback that is called when user selects a new item.
   final ValueChanged<T> onItemSelected;
 
+  /// Label of the segmented control.
   final String? label;
 
+  /// Error message that is displayed below the segmented control.
   final String? error;
 
+  /// Whether the segmented control is enabled or not.
   final bool isEnabled;
 
+  /// Whether the segmented control is required or not.
   final bool isRequired;
+
+  /// Direction of the segmented control widget.
+  final Axis direction;
 
   late final _selectedItemIndex =
       items.map((i) => i.value).toList().indexOf(value);
+
+  OptimusStackDistribution get _distribution => direction == Axis.horizontal
+      ? OptimusStackDistribution.stretch
+      : OptimusStackDistribution.basic;
+
+  OptimusStackSpacing get _spacing => direction == Axis.horizontal
+      ? OptimusStackSpacing.spacing0
+      : OptimusStackSpacing.spacing50;
+
+  Widget _buildGroupWrapper(Widget child) => direction == Axis.horizontal
+      ? BorderWrapper(
+          size: size,
+          selectedItemIndex: _selectedItemIndex,
+          listSize: items.length,
+          isEnabled: isEnabled,
+          child: child,
+        )
+      : child;
+
+  Widget _buildChildWrapper(int index, OptimusGroupItem<T> value) =>
+      direction == Axis.vertical
+          ? _SegmentDecoration(
+              isEnabled: isEnabled,
+              isSelected: index == _selectedItemIndex,
+              child: _buildControlItem(value),
+            )
+          : _buildControlItem(value);
+
+  Widget _buildControlItem(OptimusGroupItem<T> item) =>
+      _OptimusSegmentedControlItem<T>(
+        value: item.value,
+        size: size,
+        groupValue: value,
+        onItemSelected: onItemSelected,
+        isEnabled: isEnabled,
+        child: item.label,
+      );
 
   @override
   Widget build(BuildContext context) => GroupWrapper(
@@ -49,24 +100,12 @@ class OptimusSegmentedControl<T> extends StatelessWidget {
         isRequired: isRequired,
         child: OptimusEnabled(
           isEnabled: isEnabled,
-          child: BorderWrapper(
-            size: size,
-            selectedItemIndex: _selectedItemIndex,
-            listSize: items.length,
-            child: OptimusStack(
-              direction: Axis.horizontal,
-              distribution: OptimusStackDistribution.stretch,
-              children: items
-                  .mapIndexed(
-                    (i, v) => _OptimusSegmentedControlItem<T>(
-                      value: v.value,
-                      size: size,
-                      groupValue: value,
-                      onItemSelected: onItemSelected,
-                      child: v.label,
-                    ),
-                  )
-                  .toList(),
+          child: _buildGroupWrapper(
+            OptimusStack(
+              direction: direction,
+              distribution: _distribution,
+              spacing: _spacing,
+              children: items.mapIndexed(_buildChildWrapper).toList(),
             ),
           ),
         ),
@@ -81,6 +120,7 @@ class _OptimusSegmentedControlItem<T> extends StatefulWidget {
     required this.value,
     required this.groupValue,
     required this.onItemSelected,
+    required this.isEnabled,
   }) : super(key: key);
 
   final Widget child;
@@ -92,6 +132,8 @@ class _OptimusSegmentedControlItem<T> extends StatefulWidget {
   final T groupValue;
 
   final ValueChanged<T> onItemSelected;
+
+  final bool isEnabled;
 
   @override
   _OptimusSegmentedControlItemState<T> createState() =>
@@ -115,6 +157,13 @@ class _OptimusSegmentedControlItemState<T>
     }
   }
 
+  Color get _iconColor =>
+      widget.isEnabled ? theme.colors.primary500 : theme.colors.neutral100;
+
+  Color? get _color => _isHovering || _isTappedDown
+      ? theme.colors.primary500t8
+      : Colors.transparent;
+
   @override
   Widget build(BuildContext context) => MouseRegion(
         onEnter: (_) => _onHoverChanged(true),
@@ -126,24 +175,30 @@ class _OptimusSegmentedControlItemState<T>
           onTapCancel: () => setState(() => _isTappedDown = false),
           child: Container(
             color: _color,
-            height: widget.size.value,
+            constraints: BoxConstraints(minHeight: widget.size.value),
+            padding: const EdgeInsets.symmetric(vertical: spacing50),
             child: Stack(
               clipBehavior: Clip.none,
+              alignment: Alignment.center,
               children: [
                 if (_isSelected)
-                  const Align(
+                  Align(
                     alignment: Alignment.centerLeft,
                     child: Padding(
-                      padding: EdgeInsets.only(left: spacing100),
-                      child: OptimusIcon(
-                        iconData: OptimusIcons.done,
-                        colorOption: OptimusIconColorOption.primary,
+                      padding: const EdgeInsets.only(left: spacing100),
+                      child: IconTheme(
+                        data: IconThemeData(
+                          color: _iconColor,
+                          size: 24,
+                        ),
+                        child: const Icon(OptimusIcons.done),
                       ),
                     ),
                   ),
                 Center(
                   child: DefaultTextStyle.merge(
                     overflow: TextOverflow.ellipsis,
+                    textAlign: TextAlign.center,
                     style: preset300b.copyWith(
                       color: theme.isDark
                           ? theme.colors.neutral0
@@ -162,8 +217,35 @@ class _OptimusSegmentedControlItemState<T>
           ),
         ),
       );
+}
 
-  Color? get _color => _isHovering || _isTappedDown
-      ? theme.colors.primary500t8
-      : Colors.transparent;
+class _SegmentDecoration extends StatelessWidget {
+  const _SegmentDecoration({
+    Key? key,
+    required this.child,
+    required this.isEnabled,
+    required this.isSelected,
+  }) : super(key: key);
+
+  final Widget child;
+  final bool isEnabled;
+  final bool isSelected;
+
+  Color _color(OptimusThemeData theme) => (isEnabled && isSelected)
+      ? theme.colors.primary500
+      : theme.colors.neutral100;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = OptimusTheme.of(context);
+
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: Colors.transparent,
+        borderRadius: const BorderRadius.all(borderRadius50),
+        border: Border.all(color: _color(theme)),
+      ),
+      child: child,
+    );
+  }
 }
--- optimus/lib/src/segmented_control/wrapper.dart
+++ optimus/lib/src/segmented_control/wrapper.dart
@@ -8,6 +8,7 @@ class BorderWrapper extends StatefulWidget {
     required this.size,
     required this.selectedItemIndex,
     required this.listSize,
+    required this.isEnabled,
   }) : super(key: key);
 
   final Widget child;
@@ -18,42 +19,13 @@ class BorderWrapper extends StatefulWidget {
 
   final int listSize;
 
+  final bool isEnabled;
+
   @override
   State<BorderWrapper> createState() => _BorderWrapperState();
 }
 
 class _BorderWrapperState extends State<BorderWrapper> with ThemeGetter {
-  @override
-  Widget build(BuildContext context) => LayoutBuilder(
-        builder: (context, constraints) => Stack(
-          children: [
-            Container(
-              height: widget.size.value,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(borderRadius50),
-                border: Border.all(color: theme.colors.neutral100),
-              ),
-              child: widget.child,
-            ),
-            Row(children: _dividers(constraints.maxWidth)),
-            Positioned(
-              left: _left(constraints.maxWidth),
-              child: IgnorePointer(
-                child: Container(
-                  width: _width(constraints.maxWidth),
-                  height: widget.size.value,
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    borderRadius: _borderRadius,
-                    border: Border.all(color: theme.colors.primary500),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-
   List<Widget> _dividers(double maxWidth) =>
       Iterable<int>.generate(widget.listSize - 1)
           .map(
@@ -122,6 +94,40 @@ class _BorderWrapperState extends State<BorderWrapper> with ThemeGetter {
 
     return _ItemPosition.inBetween;
   }
+
+  Color get _borderColor =>
+      widget.isEnabled ? theme.colors.primary500 : theme.colors.neutral100;
+
+  @override
+  Widget build(BuildContext context) => LayoutBuilder(
+        builder: (context, constraints) => Stack(
+          children: [
+            Container(
+              height: widget.size.value,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(borderRadius50),
+                border: Border.all(color: theme.colors.neutral100),
+              ),
+              child: widget.child,
+            ),
+            Row(children: _dividers(constraints.maxWidth)),
+            Positioned(
+              left: _left(constraints.maxWidth),
+              child: IgnorePointer(
+                child: Container(
+                  width: _width(constraints.maxWidth),
+                  height: widget.size.value,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: _borderRadius,
+                    border: Border.all(color: _borderColor),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
 }
 
 enum _ItemPosition { first, inBetween, last }
