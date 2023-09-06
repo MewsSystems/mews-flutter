69f57241851d8036aa577cc41074f17c4bc6da93
Vitalij Vascenko <vetal.zt@gmail.com>
2023-09-06 17:09:10 +0200

    feat: [UX-140] Update Badge component

--- optimus/lib/src/badge.dart
+++ optimus/lib/src/badge.dart
@@ -3,53 +3,76 @@ import 'package:flutter/material.dart';
 import 'package:optimus/optimus.dart';
 import 'package:optimus/src/typography/presets.dart';
 
+/// Badges are meant to give a subtle feedback about some state change.
+/// Typically used with buttons, tabs and icons.
 class OptimusBadge extends StatelessWidget {
   const OptimusBadge({
     super.key,
     this.text = '',
+    this.outline = true,
   });
 
+  /// Text of the badge. If empty, badge will be represented as a simple dot.
   final String text;
+
+  /// Whether to use the outline. Intended to be enabled when the badge is used
+  /// for example on top of the [OptimusButtonVariant.ghost]. Outlined version
+  /// could be more accessible, depending on the underlying component.
+  final bool outline;
+
+  double get _bareHeight => text.isEmpty ? _emptySize : _badgeHeight;
+  double get _outlinedHeight => _bareHeight + _outlineSize * 2;
+  double get _height => outline ? _outlinedHeight : _bareHeight;
 
   @override
   Widget build(BuildContext context) {
     final hasText = text.isNotEmpty;
-    final theme = OptimusTheme.of(context);
+    final tokens = OptimusTheme.of(context).tokens;
+    final backgroundColor = tokens.backgroundAccentPrimary;
+    final foregroundColor = tokens.textStaticInverse;
+    final outlineColor = tokens.borderStaticInverse;
     final decoration = BoxDecoration(
-      borderRadius: const BorderRadius.all(Radius.circular(10)),
-      color: theme.colors.primary,
-      border: Border.all(
-        width: 2,
-        color: theme.isDark ? theme.colors.neutral500 : theme.colors.neutral0,
-      ),
+      borderRadius: const BorderRadius.all(Radius.circular(50)),
+      color: backgroundColor,
+      border:
+          outline ? Border.all(width: _outlineSize, color: outlineColor) : null,
     );
+
+    final child = hasText
+        ? Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                text,
+                textAlign: TextAlign.center,
+                style: baseTextStyle.copyWith(
+                  fontSize: 11,
+                  letterSpacing: 0.2,
+                  height: 1,
+                  fontWeight: FontWeight.w600,
+                  color: foregroundColor,
+                ),
+              ),
+            ],
+          )
+        : null;
 
     return Container(
       constraints: BoxConstraints(
-        minWidth: hasText ? 20 : 10,
-        maxWidth: hasText ? double.infinity : 10,
+        minWidth: hasText ? _badgeHeight : _height,
+        maxWidth: hasText ? double.infinity : _height,
       ),
-      height: hasText ? 20 : 10,
+      height: _height,
       decoration: decoration,
       padding: const EdgeInsets.symmetric(
         horizontal: spacing50,
         vertical: spacing25,
       ),
-      child: IntrinsicWidth(
-        child: Center(
-          child: hasText
-              ? Text(
-                  text,
-                  textAlign: TextAlign.center,
-                  style: preset50b.copyWith(
-                    color: theme.isDark
-                        ? theme.colors.neutral1000
-                        : theme.colors.neutral0,
-                  ),
-                )
-              : const SizedBox(),
-        ),
-      ),
+      child: child,
     );
   }
 }
+
+const _emptySize = spacing100;
+const _outlineSize = 2.0;
+const _badgeHeight = 16.0;
