965e17c5aff762122c82764dd06b29953c47ecb9
Vitalij Vaščenko <vetal.zt@gmail.com>
2023-09-19 23:40:40 +0200

    fix: Fix Badge overflowing (#436)

--- optimus/lib/src/badge/badge.dart
+++ optimus/lib/src/badge/badge.dart
@@ -9,6 +9,7 @@ class OptimusBadge extends StatelessWidget {
     super.key,
     this.text = '',
     this.outline = true,
+    this.overflow = TextOverflow.ellipsis,
   });
 
   /// Text of the badge. If empty, badge will be represented as a simple dot.
@@ -18,6 +19,11 @@ class OptimusBadge extends StatelessWidget {
   /// for example on top of the [OptimusButtonVariant.ghost]. Outlined version
   /// could be more accessible, depending on the underlying component.
   final bool outline;
+
+  /// Define how to display the overflowing text. Defaults to
+  /// [TextOverflow.ellipsis]. Due to small height of the badge, the
+  /// [TextOverflow.fade] is not recommended, as it makes the badge unreadable.
+  final TextOverflow overflow;
 
   @override
   Widget build(BuildContext context) => BaseBadge(text: text, outline: outline);
