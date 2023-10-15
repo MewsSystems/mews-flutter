2f10d0b33d2e545b5da8c19dd0f3e4991a1869fe
Vitalij Vascenko <vetal.zt@gmail.com>
2023-10-16 01:37:01 +0200

    feat!: [DX-144] Update link design

---
+++ optimus/lib/src/link/link_variant.dart
@@ -0,0 +1,4 @@
+/// Variant of the link.
+/// [OptimusLinkVariant.primary] - will use indigo as a base color.
+/// [OptimusLinkVariant.basic] - will use the bare (grey) color as its base.
+enum OptimusLinkVariant { primary, basic }
