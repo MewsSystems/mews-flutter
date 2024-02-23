29135f785b7de64e8923857f5d343ff950567687
mews-tech <tech.account@mewssystems.com>
2024-02-23 13:42:27 +0000

    New dark theme tokens have been generated

--- optimus/lib/src/tokens/tokens_dark.dart
+++ optimus/lib/src/tokens/tokens_dark.dart
@@ -3,7 +3,7 @@
 //
 
 // Do not edit directly
-// Generated on Tue, 06 Feb 2024 13:21:36 GMT
+// Generated on Fri, 23 Feb 2024 13:33:32 GMT
 
 import 'dart:ui';
 import 'package:flutter/widgets.dart';
@@ -59,6 +59,7 @@ class DesignTokensDark {
   static const Color backgroundStaticFlat = Color(0xFF12121A);
   static const Color backgroundStaticFloating = Color(0xFF1A1A24);
   static const Color backgroundStaticInverse = Color(0xFFFFFFFF);
+  static const Color backgroundStaticOnColor = Color(0xFFFFFFFF);
   static const Color backgroundStaticRaised = Color(0xFF16161F);
   static const Color backgroundStaticSunken = Color(0xFF000000);
   static const Color borderAlertBasic = Color(0xFF53536D);
@@ -507,6 +508,7 @@ class DesignTokensDark {
   static const double spacing450 = 36;
   static const double spacing50 = 4;
   static const double spacing500 = 40;
+  static const double spacing600 = 48;
   static const double spacing700 = 56;
   static const double spacing900 = 72;
   static const double spacingBase = 8;
