327f6b30927985fbf9dc88ec5513552e1c6f6fd8
Vitalij Vascenko <vetal.zt@gmail.com>
2024-06-17 12:18:33 +0200

    added lint error

--- optimus/lib/src/button/base_button.dart
+++ optimus/lib/src/button/base_button.dart
@@ -4,7 +4,7 @@ import 'package:optimus/src/button/base_button_variant.dart';
 import 'package:optimus/src/button/common.dart';
 
 class BaseButton extends StatefulWidget {
-  const BaseButton({
+  BaseButton({
     super.key,
     this.onPressed,
     required this.child,
