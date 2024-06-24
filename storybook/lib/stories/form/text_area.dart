4e9122f23f4ba8980a65f1105965fd530eeff0a6
Vitalij Vascenko <vetal.zt@gmail.com>
2024-06-24 17:25:08 +0200

    feat: [DX-1801] Add TextArea component

---
+++ storybook/lib/stories/form/text_area.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+import 'package:storybook_flutter/storybook_flutter.dart';
+
+final textAreaStory = Story(
+  name: 'Forms/Text Area',
+  builder: (context) => const Center(),
+);
