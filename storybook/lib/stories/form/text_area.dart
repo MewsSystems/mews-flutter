da4c2fb57ab80bd9acdf679c998ae6518ea28d3e
Vitalij Vascenko <vetal.zt@gmail.com>
2024-07-24 15:52:42 +0200

    upd story

--- storybook/lib/stories/form/text_area.dart
+++ storybook/lib/stories/form/text_area.dart
@@ -8,21 +8,28 @@ final textAreaStory = Story(
     final k = context.knobs;
 
     final label = k.text(label: 'Label', initial: 'Label');
+    final placeholder = k.text(label: 'Placeholder', initial: 'Placeholder');
+    final error = k.text(label: 'Error', initial: '');
     final isEnabled = k.boolean(label: 'Enabled', initial: true);
     final isRequired = k.boolean(label: 'Required', initial: false);
     final rows = k.sliderInt(label: 'Rows', initial: 1, min: 1, max: 10);
     final autoCollapse = k.boolean(label: 'Auto collapse', initial: true);
     final autoSize = k.boolean(label: 'Auto size', initial: true);
+    final maxCharacters =
+        k.sliderInt(label: 'Max characters', initial: 100, min: 0, max: 100);
 
     return Padding(
       padding: EdgeInsets.all(context.tokens.spacing100),
       child: OptimusTextArea(
         label: label,
+        placeholder: placeholder,
         isEnabled: isEnabled,
         isRequired: isRequired,
         rows: rows,
         autoCollapse: autoCollapse,
         autoSize: autoSize,
+        error: error.isNotEmpty ? error : null,
+        maxCharacters: maxCharacters == 0 ? null : maxCharacters,
       ),
     );
   },
