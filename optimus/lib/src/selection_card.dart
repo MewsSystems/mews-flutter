42a94f42f07fade87f12aff0fcbbb53ce289012c
Vitalij Vascenko <vetal.zt@gmail.com>
2024-08-12 11:17:12 +0200

    upd style

--- optimus/lib/src/selection_card.dart
+++ optimus/lib/src/selection_card.dart
@@ -36,51 +36,132 @@ class _OptimusSelectionCardState extends State<OptimusSelectionCard>
     with ThemeGetter {
   bool _isHovered = false;
   bool _isPressed = false;
+  final _controller = WidgetStatesController();
 
   @override
-  Widget build(BuildContext context) => GestureWrapper(
-        onHoverChanged: (isHovered) {
-          setState(() {
-            _isHovered = isHovered;
-          });
-        },
-        onPressedChanged: (isPressed) {
-          setState(() {
-            _isPressed = isPressed;
-          });
-        },
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-            borderRadius:
-                BorderRadius.all(widget.borderRadius.getBorderRadius(tokens)),
-            border: Border.all(
-              color: context.tokens
-                  .borderInteractiveSecondaryDefault, // TODO(witwash): replace
-              width: context.tokens.borderWidth150,
-            ),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              RadioCircle(
-                state: RadioState.basic,
-                isSelected: widget.isSelected,
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  _InteractiveStateColor get _backgroundColor => _InteractiveStateColor(
+        context.tokens.backgroundStaticFlat.value,
+        disabled: context.tokens.backgroundStaticFlat,
+        pressed: context.tokens.backgroundStaticFlat,
+        hovered: context.tokens.backgroundStaticFlat,
+      );
+
+  _InteractiveStateColor get _borderColor => _InteractiveStateColor(
+        context.tokens.borderInteractiveSecondaryDefault.value,
+        disabled: context.tokens.borderDisabled,
+        pressed: context.tokens.borderInteractiveSecondaryActive,
+        hovered: context.tokens.borderInteractiveSecondaryHover,
+      );
+
+  _InteractiveStateColor get _titleColor => _InteractiveStateColor(
+        context.tokens.textStaticPrimary.value,
+        disabled: context.tokens.textDisabled,
+        pressed: context.tokens.textStaticPrimary,
+        hovered: context.tokens.textStaticPrimary,
+      );
+
+  _InteractiveStateColor get _descriptionColor => _InteractiveStateColor(
+        context.tokens.textStaticTertiary.value,
+        disabled: context.tokens.textDisabled,
+        pressed: context.tokens.textStaticTertiary,
+        hovered: context.tokens.textStaticTertiary,
+      );
+
+  @override
+  void didUpdateWidget(covariant OptimusSelectionCard oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.isEnabled != oldWidget.isEnabled) {
+      _controller.update(WidgetState.disabled, !widget.isEnabled);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => ListenableBuilder(
+        listenable: _controller,
+        builder: (context, _) {
+          final backgroundColor = _backgroundColor.resolve(_controller.value);
+          final borderColor = _borderColor.resolve(_controller.value);
+          final titleColor = _titleColor.resolve(_controller.value);
+          final descriptionColor = _descriptionColor.resolve(_controller.value);
+
+          return GestureWrapper(
+            onHoverChanged: (isHovered) =>
+                _controller.update(WidgetState.hovered, isHovered),
+            onPressedChanged: (isPressed) =>
+                _controller.update(WidgetState.pressed, isPressed),
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: backgroundColor,
+                borderRadius: BorderRadius.all(
+                  widget.borderRadius.getBorderRadius(tokens),
+                ),
+                border: Border.all(
+                  color: borderColor, // TODO(witwash): replace
+                  width: context.tokens.borderWidth150,
+                ),
               ),
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  OptimusTitleMedium(child: widget.title),
-                  if (widget.description case final description?)
-                    OptimusSubtitle(child: description),
+                  RadioCircle(
+                    state: RadioState.basic,
+                    isSelected: widget.isSelected,
+                  ),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      DefaultTextStyle.merge(
+                        child: widget.title,
+                        style:
+                            tokens.bodyLargeStrong.copyWith(color: titleColor),
+                      ),
+                      if (widget.description case final description?)
+                        DefaultTextStyle.merge(
+                            child: description,
+                            style: tokens.bodyMedium
+                                .copyWith(color: descriptionColor)),
+                    ],
+                  ),
+                  const Spacer(),
+                  if (widget.trailing case final trailing?) trailing,
                 ],
               ),
-              const Spacer(),
-              if (widget.trailing case final trailing?) trailing,
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       );
+}
+
+class _InteractiveStateColor extends WidgetStateColor {
+  const _InteractiveStateColor(super.defaultValue,
+      {required this.disabled, required this.pressed, required this.hovered})
+      : _defaultColor = defaultValue;
+
+  final Color disabled;
+  final Color pressed;
+  final Color hovered;
+  final int _defaultColor;
+
+  @override
+  Color resolve(Set<WidgetState> states) {
+    if (states.isDisabled) return disabled;
+    if (states.isPressed) return pressed;
+    if (states.isHovered) return hovered;
+
+    return Color(_defaultColor);
+  }
+}
+
+extension on Set<WidgetState> {
+  bool get isDisabled => contains(WidgetState.disabled);
+  bool get isPressed => contains(WidgetState.pressed);
+  bool get isHovered => contains(WidgetState.hovered);
 }
 
 extension on OptimusSelectionCardBorderRadius {
