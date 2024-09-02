47f381d1af86cfa91db5e526d0420379872f0ccd
Vitalij Vascenko <vetal.zt@gmail.com>
2024-09-02 11:04:52 +0200

    refactor: Remove storybook all icons list

--- storybook/lib/stories/icon/icons.dart
+++ storybook/lib/stories/icon/icons.dart
@@ -1,26 +1,8 @@
 import 'package:flutter/widgets.dart';
-import 'package:optimus/optimus.dart';
-import 'package:storybook/stories/icon/icons_list.dart';
 import 'package:storybook_flutter/storybook_flutter.dart';
 
 final Story allIconsStory = Story(
   name: 'Media/Icons/All Icons',
-  builder: (context) => GridView.builder(
-    itemCount: optimusIcons.length,
-    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-      crossAxisCount: 5,
-    ),
-    itemBuilder: (context, index) => Padding(
-      padding: const EdgeInsets.all(8),
-      child: Column(
-        children: [
-          OptimusIcon(iconData: optimusIcons[index].data),
-          Text(
-            optimusIcons[index].name,
-            overflow: TextOverflow.ellipsis,
-          ),
-        ],
-      ),
-    ),
-  ),
+  builder: (context) =>
+      const Center(), // TODO(witwash): return after new optimus_icons is released
 );
