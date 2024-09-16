33b2e3d1e743369fda942a5de27995b82b1325bf
Vitalij Vascenko <vetal.zt@gmail.com>
2024-09-16 23:47:14 +0200

    feat: [DX-2198] Migrate stories to widgetbook

---
+++ optimus_widgetbook/lib/common.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+import 'package:optimus/optimus.dart';
+
+final List<IconData?> exampleIcons = [
+  null,
+  OptimusIcons.mews_logo,
+  OptimusIcons.magic,
+  OptimusIcons.plus,
+  OptimusIcons.delete,
+  OptimusIcons.edit,
+  OptimusIcons.chevron_right,
+  OptimusIcons.chevron_left,
+];
