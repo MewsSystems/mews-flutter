299dab864605e825aa915e3a6a11f59462798f6d
Vitalij Vascenko <vetal.zt@gmail.com>
2024-09-30 12:47:36 +0200

    refactor: Remove storybook and update references

--- storybook/README.md
+++
@@ -1,5 +0,0 @@
-![](https://github.com/MewsSystems/mews-flutter/workflows/Check%20Storybook/badge.svg)
-
-# Optimus Storybook
-
-Storybook for [Optimus](../optimus/) package. Check https://mews-optimus.web.app/ for the latest version.
--- storybook/lib/stories/icon/icons.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/widgets.dart';
-import 'package:storybook_flutter/storybook_flutter.dart';
-
-final Story allIconsStory = Story(
-  name: 'Media/Icons/All Icons',
-  builder: (context) =>
-      const Center(), // TODO(witwash): return after new optimus_icons is released
-);
