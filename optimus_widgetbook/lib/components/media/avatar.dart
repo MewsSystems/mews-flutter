308470bbedae747da56b8c8514762823c8e8abcf
Vitalij Vascenko <vetal.zt@gmail.com>
2025-02-08 20:26:38 +0100

    lint

--- optimus_widgetbook/lib/components/media/avatar.dart
+++ optimus_widgetbook/lib/components/media/avatar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:optimus/optimus.dart';
 import 'package:optimus_widgetbook/utils.dart';
 import 'package:widgetbook/widgetbook.dart';
