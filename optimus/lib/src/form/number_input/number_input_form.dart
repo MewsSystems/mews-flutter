43cb5dc3b3a1cbe0bba76958ab544612f28547c9
Vitalij Vaščenko <vetal.zt@gmail.com>
2025-02-27 15:28:12 +0100

    feat: [DX-3779] Add OptimusNumberInput component (#747)

---
+++ optimus/lib/src/form/number_input/number_input_form.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/widgets.dart';
+import 'package:optimus/optimus.dart';
+import 'package:optimus/src/form/number_input/string_formatter.dart';
+
+/// A form field that allows the user to input a number.
+/// The number is formatted according to the provided [precision] and
+/// [separatorVariant]. See [OptimusNumberSeparatorVariant] for available
+/// separator variants and [OptimusNumberInput] for more details.
+class OptimusNumberInputFormField extends FormField<String> {
+  OptimusNumberInputFormField({
+    super.key,
+    double? initialValue,
+    this.controller,
+    super.onSaved,
+    super.validator,
+    super.autovalidateMode,
+    String? placeholder,
+    bool isEnabled = true,
+    ValueChanged<String>? onSubmitted,
+    FocusNode? focusNode,
+    String? label,
+    bool isRequired = false,
+    Widget? suffix,
+    Widget? prefix,
+    bool readOnly = false,
+    Widget? helperMessage,
+    OptimusWidgetSize size = OptimusWidgetSize.large,
+    bool showLoader = false,
+    this.precision = 2,
+    this.separatorVariant = OptimusNumberSeparatorVariant.commaAndStop,
+    this.min = 0,
+    double max = 12,
+    bool allowNegate = false,
+    bool isInlined = false,
+    double step = 1,
+  })  : assert(
+          initialValue == null || controller == null,
+          'Provide either initial value or controller',
+        ),
+        super(
+          initialValue: controller != null
+              ? controller.text.format(
+                  precision: precision,
+                  separatorVariant: separatorVariant,
+                )
+              : (initialValue?.toString().format(
+                        precision: precision,
+                        separatorVariant: separatorVariant,
+                      ) ??
+                  min.toString().format(
+                        precision: precision,
+                        separatorVariant: separatorVariant,
+                      )),
+          enabled: isEnabled,
+          builder: (FormFieldState<String> field) {
+            // ignore: avoid-type-casts, can't be anything else. No need to check
+            final _InputFormFieldState state = field as _InputFormFieldState;
+
+            return OptimusNumberInput(
+              onChanged: field.didChange,
+              placeholder: placeholder,
+              isEnabled: isEnabled,
+              onSubmitted: onSubmitted,
+              focusNode: focusNode,
+              label: label,
+              controller: state._effectiveController,
+              error: field.errorText,
+              isRequired: isRequired,
+              prefix: prefix,
+              suffix: suffix,
+              isReadOnly: readOnly,
+              helperMessage: helperMessage,
+              size: size,
+              showLoader: showLoader,
+              precision: precision,
+              separatorVariant: separatorVariant,
+              min: min,
+              max: max,
+              allowNegate: allowNegate,
+              isInlined: isInlined,
+              step: step,
+            );
+          },
+        );
+
+  final TextEditingController? controller;
+  final int precision;
+  final double min;
+  final OptimusNumberSeparatorVariant separatorVariant;
+
+  @override
+  FormFieldState<String> createState() => _InputFormFieldState();
+}
+
+class _InputFormFieldState extends FormFieldState<String> {
+  TextEditingController? _controller;
+
+  TextEditingController get _effectiveController =>
+      widget.controller ??
+      _controller!; // ignore: avoid-non-null-assertion, _controller would be
+  // initialized at this point
+
+  @override
+  OptimusNumberInputFormField get widget =>
+      // ignore: avoid-type-casts, can't be anything else. No need to check.
+      super.widget as OptimusNumberInputFormField;
+
+  @override
+  void initState() {
+    super.initState();
+    final controller = widget.controller;
+    if (controller == null) {
+      _controller = TextEditingController(
+        text: _formatValue(widget.initialValue ?? _fallbackValue),
+      );
+    } else {
+      controller.addListener(_handleControllerChanged);
+    }
+  }
+
+  @override
+  void didUpdateWidget(OptimusNumberInputFormField oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.controller != oldWidget.controller) {
+      oldWidget.controller?.removeListener(_handleControllerChanged);
+      widget.controller?.addListener(_handleControllerChanged);
+
+      final oldWidgetController = oldWidget.controller;
+      final widgetController = widget.controller;
+
+      if (oldWidgetController != null && widgetController == null) {
+        _controller =
+            TextEditingController.fromValue(oldWidgetController.value);
+      }
+      if (widgetController != null) {
+        setValue(_formatValue(widgetController.text));
+        if (oldWidgetController == null) _controller = null;
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.controller?.removeListener(_handleControllerChanged);
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  String _formatValue(String value) => value.format(
+        precision: widget.precision,
+        separatorVariant: widget.separatorVariant,
+      );
+
+  String get _fallbackValue => widget.min.toString();
+
+  @override
+  void didChange(String? value) {
+    super.didChange(value);
+
+    if (_effectiveController.text != value) {
+      _effectiveController.text = _formatValue(value ?? _fallbackValue);
+    }
+  }
+
+  @override
+  void reset() {
+    super.reset();
+    setState(() {
+      _effectiveController.text =
+          _formatValue(widget.initialValue ?? _fallbackValue);
+    });
+  }
+
+  void _handleControllerChanged() {
+    // Suppress changes that originated from within this class.
+    //
+    // In the case where a controller has been passed in to this widget, we
+    // register this change listener. In these cases, we'll also receive change
+    // notifications for changes originating from within this class -- for
+    // example, the reset() method. In such cases, the FormField value will
+    // already have been set.
+    if (_effectiveController.text != value) {
+      didChange(_effectiveController.text);
+    }
+  }
+}
--- optimus_widgetbook/lib/main.directories.g.dart
+++ optimus_widgetbook/lib/main.directories.g.dart
@@ -43,48 +43,51 @@ import 'package:optimus_widgetbook/components/forms/date_time_field.dart'
     as _i30;
 import 'package:optimus_widgetbook/components/forms/form_story.dart' as _i27;
 import 'package:optimus_widgetbook/components/forms/input.dart' as _i31;
-import 'package:optimus_widgetbook/components/forms/password_form.dart' as _i32;
-import 'package:optimus_widgetbook/components/forms/radio.dart' as _i33;
-import 'package:optimus_widgetbook/components/forms/search_field.dart' as _i34;
+import 'package:optimus_widgetbook/components/forms/number_input.dart' as _i32;
+import 'package:optimus_widgetbook/components/forms/number_input_form.dart'
+    as _i33;
+import 'package:optimus_widgetbook/components/forms/password_form.dart' as _i34;
+import 'package:optimus_widgetbook/components/forms/radio.dart' as _i35;
+import 'package:optimus_widgetbook/components/forms/search_field.dart' as _i36;
 import 'package:optimus_widgetbook/components/forms/segmented_control.dart'
-    as _i35;
-import 'package:optimus_widgetbook/components/forms/select_input.dart' as _i36;
-import 'package:optimus_widgetbook/components/forms/selection_card.dart'
     as _i37;
-import 'package:optimus_widgetbook/components/forms/stepper.dart' as _i38;
-import 'package:optimus_widgetbook/components/forms/text_area.dart' as _i39;
-import 'package:optimus_widgetbook/components/forms/toggle.dart' as _i40;
-import 'package:optimus_widgetbook/components/helpers/slidable.dart' as _i41;
-import 'package:optimus_widgetbook/components/icon/icon.dart' as _i49;
-import 'package:optimus_widgetbook/components/icon/icon_list.dart' as _i50;
-import 'package:optimus_widgetbook/components/icon/icons.dart' as _i48;
-import 'package:optimus_widgetbook/components/layout/card.dart' as _i42;
-import 'package:optimus_widgetbook/components/layout/dialog.dart' as _i44;
-import 'package:optimus_widgetbook/components/layout/divider.dart' as _i46;
+import 'package:optimus_widgetbook/components/forms/select_input.dart' as _i38;
+import 'package:optimus_widgetbook/components/forms/selection_card.dart'
+    as _i39;
+import 'package:optimus_widgetbook/components/forms/stepper.dart' as _i40;
+import 'package:optimus_widgetbook/components/forms/text_area.dart' as _i41;
+import 'package:optimus_widgetbook/components/forms/toggle.dart' as _i42;
+import 'package:optimus_widgetbook/components/helpers/slidable.dart' as _i43;
+import 'package:optimus_widgetbook/components/icon/icon.dart' as _i51;
+import 'package:optimus_widgetbook/components/icon/icon_list.dart' as _i52;
+import 'package:optimus_widgetbook/components/icon/icons.dart' as _i50;
+import 'package:optimus_widgetbook/components/layout/card.dart' as _i44;
+import 'package:optimus_widgetbook/components/layout/dialog.dart' as _i46;
+import 'package:optimus_widgetbook/components/layout/divider.dart' as _i48;
 import 'package:optimus_widgetbook/components/layout/inline_dialog.dart'
-    as _i45;
-import 'package:optimus_widgetbook/components/layout/spacing.dart' as _i43;
-import 'package:optimus_widgetbook/components/layout/stack.dart' as _i47;
-import 'package:optimus_widgetbook/components/link/inline_link.dart' as _i54;
+    as _i47;
+import 'package:optimus_widgetbook/components/layout/spacing.dart' as _i45;
+import 'package:optimus_widgetbook/components/layout/stack.dart' as _i49;
+import 'package:optimus_widgetbook/components/link/inline_link.dart' as _i56;
 import 'package:optimus_widgetbook/components/link/standalone_link.dart'
-    as _i56;
+    as _i58;
 import 'package:optimus_widgetbook/components/list/expanded_list.dart' as _i8;
 import 'package:optimus_widgetbook/components/list/list_tile.dart' as _i9;
 import 'package:optimus_widgetbook/components/list/nav_list_tile.dart' as _i10;
-import 'package:optimus_widgetbook/components/media/avatar.dart' as _i51;
-import 'package:optimus_widgetbook/components/media/logo.dart' as _i52;
-import 'package:optimus_widgetbook/components/media/pictogram.dart' as _i53;
+import 'package:optimus_widgetbook/components/media/avatar.dart' as _i53;
+import 'package:optimus_widgetbook/components/media/logo.dart' as _i54;
+import 'package:optimus_widgetbook/components/media/pictogram.dart' as _i55;
 import 'package:optimus_widgetbook/components/navigation/progress_indicator.dart'
-    as _i55;
+    as _i57;
 import 'package:optimus_widgetbook/components/tab/tab.dart' as _i11;
 import 'package:optimus_widgetbook/components/tab/tabs.dart' as _i12;
-import 'package:optimus_widgetbook/components/typography/caption.dart' as _i59;
+import 'package:optimus_widgetbook/components/typography/caption.dart' as _i61;
 import 'package:optimus_widgetbook/components/typography/highlight.dart'
-    as _i57;
-import 'package:optimus_widgetbook/components/typography/label.dart' as _i58;
+    as _i59;
+import 'package:optimus_widgetbook/components/typography/label.dart' as _i60;
 import 'package:optimus_widgetbook/components/typography/paragraph.dart'
-    as _i61;
-import 'package:optimus_widgetbook/components/typography/title.dart' as _i60;
+    as _i63;
+import 'package:optimus_widgetbook/components/typography/title.dart' as _i62;
 import 'package:widgetbook/widgetbook.dart' as _i1;
 
 final directories = <_i1.WidgetbookNode>[
@@ -362,24 +365,38 @@ final directories = <_i1.WidgetbookNode>[
         ),
       ),
       _i1.WidgetbookLeafComponent(
+        name: 'OptimusNumberInput',
+        useCase: _i1.WidgetbookUseCase(
+          name: 'NumberInput',
+          builder: _i32.createDefaultStyle,
+        ),
+      ),
+      _i1.WidgetbookLeafComponent(
+        name: 'OptimusNumberInputFormField',
+        useCase: _i1.WidgetbookUseCase(
+          name: 'NumberInputForm',
+          builder: _i33.createDefaultStyle,
+        ),
+      ),
+      _i1.WidgetbookLeafComponent(
         name: 'OptimusPasswordFormField',
         useCase: _i1.WidgetbookUseCase(
           name: 'Password',
-          builder: _i32.createDefaultStyle,
+          builder: _i34.createDefaultStyle,
         ),
       ),
       _i1.WidgetbookLeafComponent(
         name: 'OptimusRadio',
         useCase: _i1.WidgetbookUseCase(
           name: 'Radio',
-          builder: _i33.createDefaultStyle,
+          builder: _i35.createDefaultStyle,
         ),
       ),
       _i1.WidgetbookLeafComponent(
         name: 'OptimusRadioGroup',
         useCase: _i1.WidgetbookUseCase(
           name: 'Radio Group',
-          builder: _i33.createRadioGroup,
+          builder: _i35.createRadioGroup,
         ),
       ),
       _i1.WidgetbookComponent(
@@ -387,11 +404,11 @@ final directories = <_i1.WidgetbookNode>[
         useCases: [
           _i1.WidgetbookUseCase(
             name: 'Nested Search Field',
-            builder: _i34.createNestedStyle,
+            builder: _i36.createNestedStyle,
           ),
           _i1.WidgetbookUseCase(
             name: 'Search Field',
-            builder: _i34.createDefaultStyle,
+            builder: _i36.createDefaultStyle,
           ),
         ],
       ),
@@ -399,7 +416,7 @@ final directories = <_i1.WidgetbookNode>[
         name: 'OptimusSegmentedControl',
         useCase: _i1.WidgetbookUseCase(
           name: 'Segmented Control',
-          builder: _i35.createDefaultStyle,
+          builder: _i37.createDefaultStyle,
         ),
       ),
       _i1.WidgetbookComponent(
@@ -407,11 +424,11 @@ final directories = <_i1.WidgetbookNode>[
         useCases: [
           _i1.WidgetbookUseCase(
             name: 'Nested Select',
-            builder: _i36.createNestedStyle,
+            builder: _i38.createNestedStyle,
           ),
           _i1.WidgetbookUseCase(
             name: 'Select Input',
-            builder: _i36.createDefaultStyle,
+            builder: _i38.createDefaultStyle,
           ),
         ],
       ),
@@ -419,28 +436,28 @@ final directories = <_i1.WidgetbookNode>[
         name: 'OptimusSelectionCard',
         useCase: _i1.WidgetbookUseCase(
           name: 'Selection Card',
-          builder: _i37.createDefaultStyle,
+          builder: _i39.createDefaultStyle,
         ),
       ),
       _i1.WidgetbookLeafComponent(
         name: 'OptimusStepperFormField',
         useCase: _i1.WidgetbookUseCase(
           name: 'Stepper',
-          builder: _i38.createDefaultStyle,
+          builder: _i40.createDefaultStyle,
         ),
       ),
       _i1.WidgetbookLeafComponent(
         name: 'OptimusTextArea',
         useCase: _i1.WidgetbookUseCase(
           name: 'Text Area',
-          builder: _i39.createDefaultStyle,
+          builder: _i41.createDefaultStyle,
         ),
       ),
       _i1.WidgetbookLeafComponent(
         name: 'OptimusToggle',
         useCase: _i1.WidgetbookUseCase(
           name: 'Toggle',
-          builder: _i40.createDefaultStyle,
+          builder: _i42.createDefaultStyle,
         ),
       ),
     ],
@@ -452,7 +469,7 @@ final directories = <_i1.WidgetbookNode>[
         name: 'OptimusSlideAction',
         useCase: _i1.WidgetbookUseCase(
           name: 'Slidable',
-          builder: _i41.createDefaultStyle,
+          builder: _i43.createDefaultStyle,
         ),
       )
     ],
@@ -467,14 +484,14 @@ final directories = <_i1.WidgetbookNode>[
             name: 'OptimusCard',
             useCase: _i1.WidgetbookUseCase(
               name: 'Card',
-              builder: _i42.createDefaultStyle,
+              builder: _i44.createDefaultStyle,
             ),
           ),
           _i1.WidgetbookLeafComponent(
             name: 'OptimusNestedCard',
             useCase: _i1.WidgetbookUseCase(
               name: 'Nested Card',
-              builder: _i42.createNestedCard,
+              builder: _i44.createNestedCard,
             ),
           ),
         ],
@@ -483,7 +500,7 @@ final directories = <_i1.WidgetbookNode>[
         name: 'Column',
         useCase: _i1.WidgetbookUseCase(
           name: 'Spacing',
-          builder: _i43.createDefaultStyle,
+          builder: _i45.createDefaultStyle,
         ),
       ),
       _i1.WidgetbookFolder(
@@ -493,14 +510,14 @@ final directories = <_i1.WidgetbookNode>[
             name: 'OptimusDialog',
             useCase: _i1.WidgetbookUseCase(
               name: 'Modal Dialog',
-              builder: _i44.createDefaultStyle,
+              builder: _i46.createDefaultStyle,
             ),
           ),
           _i1.WidgetbookLeafComponent(
             name: 'OptimusInlineDialog',
             useCase: _i1.WidgetbookUseCase(
               name: 'Inline Dialog',
-              builder: _i45.createDefaultStyle,
+              builder: _i47.createDefaultStyle,
             ),
           ),
         ],
@@ -509,14 +526,14 @@ final directories = <_i1.WidgetbookNode>[
         name: 'OptimusDivider',
         useCase: _i1.WidgetbookUseCase(
           name: 'Divider',
-          builder: _i46.createDefaultStyle,
+          builder: _i48.createDefaultStyle,
         ),
       ),
       _i1.WidgetbookLeafComponent(
         name: 'OptimusStack',
         useCase: _i1.WidgetbookUseCase(
           name: 'Stack',
-          builder: _i47.createDefaultStyle,
+          builder: _i49.createDefaultStyle,
         ),
       ),
     ],
@@ -531,21 +548,21 @@ final directories = <_i1.WidgetbookNode>[
             name: 'GridView',
             useCase: _i1.WidgetbookUseCase(
               name: 'All Icons',
-              builder: _i48.createDefaultStyle,
+              builder: _i50.createDefaultStyle,
             ),
           ),
           _i1.WidgetbookLeafComponent(
             name: 'OptimusIcon',
             useCase: _i1.WidgetbookUseCase(
               name: 'Icon',
-              builder: _i49.createDefaultStyle,
+              builder: _i51.createDefaultStyle,
             ),
           ),
           _i1.WidgetbookLeafComponent(
             name: 'OptimusIconList',
             useCase: _i1.WidgetbookUseCase(
               name: 'Icon List',
-              builder: _i50.createDefaultStyle,
+              builder: _i52.createDefaultStyle,
             ),
           ),
         ],
@@ -554,21 +571,21 @@ final directories = <_i1.WidgetbookNode>[
         name: 'OptimusAvatar',
         useCase: _i1.WidgetbookUseCase(
           name: 'Avatar',
-          builder: _i51.createDefaultStyle,
+          builder: _i53.createDefaultStyle,
         ),
       ),
       _i1.WidgetbookLeafComponent(
         name: 'OptimusMewsLogo',
         useCase: _i1.WidgetbookUseCase(
           name: 'Logo',
-          builder: _i52.createDefaultStyle,
+          builder: _i54.createDefaultStyle,
         ),
       ),
       _i1.WidgetbookLeafComponent(
         name: 'OptimusPictogram',
         useCase: _i1.WidgetbookUseCase(
           name: 'Pictogram',
-          builder: _i53.createDefaultStyle,
+          builder: _i55.createDefaultStyle,
         ),
       ),
     ],
@@ -580,21 +597,21 @@ final directories = <_i1.WidgetbookNode>[
         name: 'OptimusInlineLink',
         useCase: _i1.WidgetbookUseCase(
           name: 'Inline Link',
-          builder: _i54.createDefaultStyle,
+          builder: _i56.createDefaultStyle,
         ),
       ),
       _i1.WidgetbookLeafComponent(
         name: 'OptimusProgressIndicator',
         useCase: _i1.WidgetbookUseCase(
           name: 'Progress Indicator',
-          builder: _i55.createDefaultStyle,
+          builder: _i57.createDefaultStyle,
         ),
       ),
       _i1.WidgetbookLeafComponent(
         name: 'OptimusStandaloneLink',
         useCase: _i1.WidgetbookUseCase(
           name: 'Standalone Link',
-          builder: _i56.createDefaultStyle,
+          builder: _i58.createDefaultStyle,
         ),
       ),
     ],
@@ -609,21 +626,21 @@ final directories = <_i1.WidgetbookNode>[
             name: 'OptimusHighlightLarge',
             useCase: _i1.WidgetbookUseCase(
               name: 'Highlight Large',
-              builder: _i57.createHighlightLarge,
+              builder: _i59.createHighlightLarge,
             ),
           ),
           _i1.WidgetbookLeafComponent(
             name: 'OptimusHighlightMedium',
             useCase: _i1.WidgetbookUseCase(
               name: 'Highlight Medium',
-              builder: _i57.createHighlightMedium,
+              builder: _i59.createHighlightMedium,
             ),
           ),
           _i1.WidgetbookLeafComponent(
             name: 'OptimusHighlightSmall',
             useCase: _i1.WidgetbookUseCase(
               name: 'Highlight Small',
-              builder: _i57.createHighLightSmall,
+              builder: _i59.createHighLightSmall,
             ),
           ),
         ],
@@ -635,14 +652,14 @@ final directories = <_i1.WidgetbookNode>[
             name: 'OptimusLabel',
             useCase: _i1.WidgetbookUseCase(
               name: 'Label',
-              builder: _i58.createLabel,
+              builder: _i60.createLabel,
             ),
           ),
           _i1.WidgetbookLeafComponent(
             name: 'OptimusLabelSmall',
             useCase: _i1.WidgetbookUseCase(
               name: 'Label Small',
-              builder: _i58.createSmallLabel,
+              builder: _i60.createSmallLabel,
             ),
           ),
         ],
@@ -651,14 +668,14 @@ final directories = <_i1.WidgetbookNode>[
         name: 'OptimusCaption',
         useCase: _i1.WidgetbookUseCase(
           name: 'Caption',
-          builder: _i59.createDefaultStyle,
+          builder: _i61.createDefaultStyle,
         ),
       ),
       _i1.WidgetbookLeafComponent(
         name: 'OptimusSubtitle',
         useCase: _i1.WidgetbookUseCase(
           name: 'Subtitle',
-          builder: _i60.createLabel,
+          builder: _i62.createLabel,
         ),
       ),
       _i1.WidgetbookFolder(
@@ -668,14 +685,14 @@ final directories = <_i1.WidgetbookNode>[
             name: 'OptimusParagraph',
             useCase: _i1.WidgetbookUseCase(
               name: 'Paragraph',
-              builder: _i61.createLabel,
+              builder: _i63.createLabel,
             ),
           ),
           _i1.WidgetbookLeafComponent(
             name: 'OptimusParagraphSmall',
             useCase: _i1.WidgetbookUseCase(
               name: 'Paragraph Small',
-              builder: _i61.createParagraphSmall,
+              builder: _i63.createParagraphSmall,
             ),
           ),
         ],
@@ -687,21 +704,21 @@ final directories = <_i1.WidgetbookNode>[
             name: 'OptimusTitleLarge',
             useCase: _i1.WidgetbookUseCase(
               name: 'Title Large',
-              builder: _i60.createTitleLarge,
+              builder: _i62.createTitleLarge,
             ),
           ),
           _i1.WidgetbookLeafComponent(
             name: 'OptimusTitleMedium',
             useCase: _i1.WidgetbookUseCase(
               name: 'Title Medium',
-              builder: _i60.createTitleMedium,
+              builder: _i62.createTitleMedium,
             ),
           ),
           _i1.WidgetbookLeafComponent(
             name: 'OptimusTitleSmall',
             useCase: _i1.WidgetbookUseCase(
               name: 'Title Small',
-              builder: _i60.createTitleSmall,
+              builder: _i62.createTitleSmall,
             ),
           ),
         ],
