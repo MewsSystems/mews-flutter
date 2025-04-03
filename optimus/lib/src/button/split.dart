5f7e50d01a1b9647911109951d3f7e7ee6690772
Vitalij Vaščenko <vetal.zt@gmail.com>
2025-04-03 14:22:12 +0200

    feat: [DX-6152] Updated OptimusSplitButton design (#760)

--- optimus/lib/src/button/split.dart
+++ optimus/lib/src/button/split.dart
@@ -1,15 +1,17 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:optimus/optimus.dart';
 import 'package:optimus/src/button/base_button.dart';
 import 'package:optimus/src/button/base_button_variant.dart';
 import 'package:optimus/src/button/base_dropdown_button.dart';
+import 'package:optimus/src/button/outlined_border.dart';
+import 'package:optimus/src/common/gesture_wrapper.dart';
 
 enum OptimusSplitButtonVariant { primary, secondary, tertiary }
 
 /// Split buttons have a main action and a dropdown action. The main action is
 /// on the left. An arrow on the right opens a dropdown menu with more actions
 /// related to the main action.
-class OptimusSplitButton<T> extends StatelessWidget {
+class OptimusSplitButton<T> extends StatefulWidget {
   const OptimusSplitButton({
     super.key,
     required this.child,
@@ -42,36 +44,72 @@ class OptimusSplitButton<T> extends StatelessWidget {
   final OptimusSplitButtonVariant variant;
 
   @override
+  State<OptimusSplitButton<T>> createState() => _OptimusSplitButtonState<T>();
+}
+
+class _OptimusSplitButtonState<T> extends State<OptimusSplitButton<T>> {
+  bool _isHovered = false;
+  bool _isPressed = false;
+
+  @override
   Widget build(BuildContext context) {
     final tokens = context.tokens;
     final borderRadius = tokens.borderRadius100;
+    final dividerColor = widget.variant.toButtonVariant().getBorderColor(
+      tokens,
+      isEnabled: widget.onPressed != null,
+      isPressed: _isPressed,
+      isHovered: _isHovered,
+    );
 
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        BaseButton(
-          onPressed: onPressed,
-          variant: variant.toButtonVariant(),
-          borderRadius: BorderRadius.only(
-            topLeft: borderRadius,
-            bottomLeft: borderRadius,
+    return GestureWrapper(
+      onHoverChanged: (isHovered) => setState(() => _isHovered = isHovered),
+      onPressedChanged: (isPressed) => setState(() => _isPressed = isPressed),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          BaseButton(
+            onPressed: widget.onPressed,
+            variant: widget.variant.toButtonVariant(),
+            borderRadius: BorderRadius.only(
+              topLeft: borderRadius,
+              bottomLeft: borderRadius,
+            ),
+            size: widget.size,
+            shapeBuilder:
+                (borderRadius, borderSide) => CustomOutlinedBorder(
+                  borderRadius: borderRadius,
+                  borderSide: borderSide,
+                  hasRight: false,
+                ),
+            child: widget.child,
           ),
-          size: size,
-          child: child,
-        ),
-        if (variant == OptimusSplitButtonVariant.primary)
-          const SizedBox(width: 1),
-        BaseDropDownButton(
-          items: items,
-          onItemSelected: onItemSelected,
-          variant: variant.toDropdownButtonVariant(),
-          borderRadius: BorderRadius.only(
-            topRight: borderRadius,
-            bottomRight: borderRadius,
+          SizedBox(
+            width: tokens.borderWidth150,
+            height: widget.size.getValue(tokens),
+            child: ColoredBox(color: dividerColor ?? Colors.transparent),
           ),
-          size: size,
-        ),
-      ],
+          BaseDropDownButton(
+            items: widget.items,
+            onItemSelected: widget.onItemSelected,
+            variant: widget.variant.toDropdownButtonVariant(),
+            borderRadius: BorderRadius.only(
+              topRight: borderRadius,
+              bottomRight: borderRadius,
+            ),
+            borderBuilder:
+                (color) => Border(
+                  top: BorderSide(color: color, width: tokens.borderWidth150),
+                  right: BorderSide(color: color, width: tokens.borderWidth150),
+                  bottom: BorderSide(
+                    color: color,
+                    width: tokens.borderWidth150,
+                  ),
+                ),
+            size: widget.size,
+          ),
+        ],
+      ),
     );
   }
 }
