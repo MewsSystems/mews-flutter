33cec91bf9e9461867e365310b90145c89c56b68
Vitalij Vascenko <vetal.zt@gmail.com>
2025-05-06 12:26:16 +0200

    upd

--- optimus/lib/src/form/input_field.dart
+++ optimus/lib/src/form/input_field.dart
@@ -62,6 +62,9 @@ class OptimusInputField extends StatefulWidget {
   final String? placeholder;
   final TextStyle? placeholderStyle;
 
+  /// The semantic label for the input field. If null, the [label] will be used.
+  /// If [label] is also null, the [placeholder] will be used. It is recommended
+  /// to use a localized string for better accessibility.
   final String? semanticsLabel;
 
   /// {@macro flutter.widgets.editableText.keyboardType}
@@ -311,66 +314,67 @@ class _OptimusInputFieldState extends State<OptimusInputField>
             )
             : null;
 
-    return FieldWrapper(
-      focusNode: _effectiveFocusNode,
-      isFocused: widget.isFocused,
-      isEnabled: widget.isEnabled,
-      label: widget.label,
-      caption: widget.caption,
-      captionIcon: widget.captionIcon,
-      helperMessage: widget.helperMessage,
-      error: error,
-      errorVariant: widget.errorVariant,
-      hasBorders: widget.hasBorders,
-      isRequired: widget.isRequired,
-      isInlined: widget.isInlined,
-      hasMultipleLines: widget.minLines != null,
-      inputCounter: widget.isInlined ? null : counter,
-      statusBarState: widget.statusBarState,
-      prefix:
-          _shouldShowPrefix
-              ? Prefix(prefix: widget.prefix, leading: widget.leading)
-              : null,
-      suffix:
-          _shouldShowSuffix
-              ? Suffix(
-                suffix: widget.suffix,
-                trailing: widget.trailing,
-                counter: widget.isInlined ? counter : null,
-                passwordButton:
-                    _isPasswordToggleVisible
-                        ? _PasswordButton(
-                          onTap: _handlePasswordTap,
-                          isEnabled: _isShowPasswordEnabled,
-                        )
-                        : null,
-                showLoader: widget.isEnabled ? widget.showLoader : false,
-                clearAllButton:
-                    _shouldShowClearAllButton
-                        ? _ClearAllButton(
-                          onTap: _handleClearAllTap,
-                          isEnabled: widget.isEnabled,
-                        )
-                        : null,
-                inlineError:
-                    _shouldShowInlineError && error != null
-                        ? InlineErrorTooltip(error: error)
-                        : null,
-              )
-              : null,
-      fieldBoxKey: widget.fieldBoxKey,
-      size: widget.size,
-      placeholder: _placeholder,
-      children: [
-        // ignore: avoid-flexible-outside-flex, it is wrapped in Row later
-        Expanded(
-          child: MergeSemantics(
+    return MergeSemantics(
+      child: FieldWrapper(
+        focusNode: _effectiveFocusNode,
+        isFocused: widget.isFocused,
+        isEnabled: widget.isEnabled,
+        label: widget.label,
+        caption: widget.caption,
+        captionIcon: widget.captionIcon,
+        helperMessage: widget.helperMessage,
+        error: error,
+        errorVariant: widget.errorVariant,
+        hasBorders: widget.hasBorders,
+        isRequired: widget.isRequired,
+        isInlined: widget.isInlined,
+        hasMultipleLines: widget.minLines != null,
+        inputCounter: widget.isInlined ? null : counter,
+        statusBarState: widget.statusBarState,
+        prefix:
+            _shouldShowPrefix
+                ? Prefix(prefix: widget.prefix, leading: widget.leading)
+                : null,
+        suffix:
+            _shouldShowSuffix
+                ? Suffix(
+                  suffix: widget.suffix,
+                  trailing: widget.trailing,
+                  counter: widget.isInlined ? counter : null,
+                  passwordButton:
+                      _isPasswordToggleVisible
+                          ? _PasswordButton(
+                            onTap: _handlePasswordTap,
+                            isEnabled: _isShowPasswordEnabled,
+                          )
+                          : null,
+                  showLoader: widget.isEnabled ? widget.showLoader : false,
+                  clearAllButton:
+                      _shouldShowClearAllButton
+                          ? _ClearAllButton(
+                            onTap: _handleClearAllTap,
+                            isEnabled: widget.isEnabled,
+                          )
+                          : null,
+                  inlineError:
+                      _shouldShowInlineError && error != null
+                          ? InlineErrorTooltip(error: error)
+                          : null,
+                )
+                : null,
+        fieldBoxKey: widget.fieldBoxKey,
+        size: widget.size,
+        placeholder: _placeholder,
+        // TODO(witwash): rework when https://github.com/flutter/flutter/issues/138794 is fixed
+        children: [
+          // ignore: avoid-flexible-outside-flex, it is wrapped in Row later
+          Expanded(
             child: Semantics(
-              label:
-                  widget.semanticsLabel ?? widget.label ?? widget.placeholder,
               textField: true,
               enabled: widget.isEnabled,
               readOnly: widget.isReadOnly,
+              label:
+                  widget.semanticsLabel ?? widget.label ?? widget.placeholder,
               child: CupertinoTextField(
                 key: widget.inputKey,
                 textAlign: widget.textAlign,
@@ -410,8 +414,8 @@ class _OptimusInputFieldState extends State<OptimusInputField>
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
