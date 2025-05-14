f7245f3aba546473f3619aa869818bd95c2900ef
Vitalij Vascenko <vetal.zt@gmail.com>
2025-05-14 15:52:48 +0200

    feat: [DX-6572] Update checkbox semantics

--- optimus/lib/src/common/group_item.dart
+++ optimus/lib/src/common/group_item.dart
@@ -3,8 +3,18 @@ import 'package:flutter/widgets.dart';
 /// Used with grouped items such as `OptimusRadioGroup`, `OptimusCheckboxGroup`
 /// and `OptimusSegmentedControl`.
 class OptimusGroupItem<T> {
-  const OptimusGroupItem({required this.label, required this.value});
+  const OptimusGroupItem({
+    required this.label,
+    required this.value,
+    this.semanticLabel,
+  });
 
+  /// The label widget of the item. Will be displayed alongside the item.
   final Widget label;
+
+  /// The semantic label of the item. Will be used for accessibility purposes.
+  final String? semanticLabel;
+
+  /// The value of the item. Will be used to identify the item.
   final T value;
 }
