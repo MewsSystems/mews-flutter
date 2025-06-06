8c4d6418e75325f130b1b2d671ebc9c8d968f298
Vitalij Vascenko <vetal.zt@gmail.com>
2025-06-06 15:27:46 +0200

    upd

--- optimus_widgetbook/lib/branding/home_page.dart
+++ optimus_widgetbook/lib/branding/home_page.dart
@@ -14,53 +14,74 @@ class HomePage extends StatelessWidget {
           builder: (context, constraints) {
             final tokens = context.tokens;
 
-            return Center(
-              child: SizedBox(
-                width: constraints.maxWidth * 0.8,
-                height: constraints.maxHeight * 0.8,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const OptimusTitleLarge(
-                      child: Text(
-                        'Welcome to the Mews Design System Widgetbook',
-                      ),
-                    ),
-                    SizedBox(height: tokens.spacing200),
-                    const OptimusTitleMedium(
-                      child: Text(
-                        'Presenting the components used across Mews products',
-                      ),
-                    ),
-                    const OptimusLabel(
-                      child: Text(
-                        'Here, we showcase the fundamental building blocks—buttons, checkboxes, form elements, and more—that bring our products to life. These core components are designed without any specific business logic, ensuring flexibility and consistency across all Mews products.',
-                      ),
-                    ),
-                    SizedBox(height: tokens.spacing200),
-                    const OptimusTitleMedium(
-                      child: Text('Core System\'s Role'),
-                    ),
-                    const OptimusLabel(
-                      child: Text(
-                        'The Core Design System serves as the foundation for building intuitive and elegant experiences. It\'s where designers and developers come together to create seamless user journeys, all powered by a shared set of components and design tokens. As our system evolves, we invite you to explore, test, and contribute to its growth.',
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        _Card(
-                          link: _documentation,
-                          image: const _CardImage(
-                            variant: _CardImageVariant.docs,
-                          ),
-                          title: const Text('Documentation'),
-                          description: const Text(
-                            'The main source of truth for our Design System',
-                          ),
+            return SingleChildScrollView(
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.all(tokens.spacing200),
+                  child: Column(
+                    children: [
+                      const OptimusTitleLarge(
+                        child: Text(
+                          'Welcome to the Mews Design System Widgetbook',
                         ),
-                      ],
-                    ),
-                  ],
+                      ),
+                      SizedBox(height: tokens.spacing200),
+                      const OptimusTitleMedium(
+                        child: Text(
+                          'Presenting the components used across Mews products',
+                        ),
+                      ),
+                      const OptimusLabel(
+                        child: Text(
+                          'Here, we showcase the fundamental building blocks—buttons, checkboxes, form elements, and more—that bring our products to life. These core components are designed without any specific business logic, ensuring flexibility and consistency across all Mews products.',
+                        ),
+                      ),
+                      SizedBox(height: tokens.spacing200),
+                      const OptimusTitleMedium(
+                        child: Text('Core System\'s Role'),
+                      ),
+                      const OptimusLabel(
+                        child: Text(
+                          'The Core Design System serves as the foundation for building intuitive and elegant experiences. It\'s where designers and developers come together to create seamless user journeys, all powered by a shared set of components and design tokens. As our system evolves, we invite you to explore, test, and contribute to its growth.',
+                        ),
+                      ),
+                      SizedBox(height: tokens.spacing400),
+                      const Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        alignment: WrapAlignment.center,
+                        children: [
+                          _Card(
+                            link: _documentation,
+                            image: _CardImage(variant: _CardImageVariant.docs),
+                            title: Text('Documentation'),
+                            description: Text(
+                              'The main source of truth for our Design System',
+                            ),
+                          ),
+                          _Card(
+                            link: _github,
+                            image: _CardImage(
+                              variant: _CardImageVariant.github,
+                            ),
+                            title: Text('GitHub'),
+                            description: Text(
+                              'The repository with source code of our Flutter components',
+                            ),
+                          ),
+                          _Card(
+                            link: _storybook,
+                            image: _CardImage(
+                              variant: _CardImageVariant.storybook,
+                            ),
+                            title: Text('Web implementation'),
+                            description: Text(
+                              'The repository with source code of our Flutter components',
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             );
@@ -84,7 +105,7 @@ class _Card extends StatelessWidget {
   final Widget title;
   final Widget? description;
 
-  void _handleDocumentationTap() => launchUrl(Uri.parse(link));
+  void _handleTap() => launchUrl(Uri.parse(link));
 
   @override
   Widget build(BuildContext context) {
@@ -93,15 +114,19 @@ class _Card extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.all(tokens.spacing100),
       child: GestureDetector(
-        onTap: _handleDocumentationTap,
-        child: OptimusCard(
-          child: Column(
-            children: [
-              SizedBox.square(dimension: tokens.sizing600, child: image),
-              OptimusTitleMedium(child: title),
-              if (description case final description?)
-                OptimusCaption(child: description),
-            ],
+        onTap: _handleTap,
+        child: SizedBox(
+          width: MediaQuery.sizeOf(context).width * 0.2,
+          height: MediaQuery.sizeOf(context).height * 0.20,
+          child: OptimusCard(
+            child: Column(
+              children: [
+                SizedBox.square(dimension: tokens.sizing1000, child: image),
+                OptimusTitleMedium(align: TextAlign.center, child: title),
+                if (description case final description?)
+                  OptimusCaption(align: TextAlign.center, child: description),
+              ],
+            ),
           ),
         ),
       ),
@@ -112,10 +137,10 @@ class _Card extends StatelessWidget {
 enum _CardImageVariant {
   docs('docs'),
   github('github'),
-  storybook('storybook'),
-  componentsStatus('components_status');
+  storybook('storybook');
 
   const _CardImageVariant(this.fileName);
+
   final String fileName;
 }
 
@@ -134,4 +159,4 @@ class _CardImage extends StatelessWidget {
 
 const _documentation = 'https://mews.design';
 const _github = 'https://github.com/MewsSystems/mews-flutter';
-const _componentStatus = 'https://mews.design';
+const _storybook = 'https://storybook.mews.design';
