5010091027294d0f34f96c7ad0abdfe235886ffe
Vitalij Vascenko <vetal.zt@gmail.com>
2025-06-10 13:10:51 +0200

    upd

--- optimus/lib/src/logo.dart
+++ optimus/lib/src/logo.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:optimus/optimus.dart';
-import 'package:optimus/src/common/semantics.dart';
 
 /// Mews logo variant.
 ///
