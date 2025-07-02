33c8c521260550979814f6919bae26d6ddf80382
Vitalij Vascenko <vetal.zt@gmail.com>
2025-07-02 13:44:20 +0200

    upd semantics

--- optimus/lib/src/button/icon.dart
+++ optimus/lib/src/button/icon.dart
@@ -71,48 +71,50 @@ class _OptimusIconButtonState extends State<OptimusIconButton>
 
     return IgnorePointer(
       ignoring: !isEnabled,
-      child: Semantics(
-        label: widget.semanticLabel,
-        button: true,
-        enabled: _isEnabled,
-        onTap: widget.onPressed,
-        onTapHint: widget.semanticOnTapHint,
-        child: GestureWrapper(
-          onHoverChanged: _handleHoverChanged,
-          onPressedChanged: _handlePressedChanged,
+      child: MergeSemantics(
+        child: Semantics(
+          label: widget.semanticLabel,
+          button: true,
+          enabled: _isEnabled,
           onTap: widget.onPressed,
-          child: AnimatedContainer(
-            height: context.getContainerSize(widget.size),
-            width: context.getContainerSize(widget.size),
-            padding: EdgeInsets.zero,
-            decoration: BoxDecoration(
-              color: _variant.getBackgroundColor(
-                tokens,
-                isEnabled: _isEnabled,
-                isPressed: _isPressed,
-                isHovered: _isHovered,
-              ),
-              border:
-                  borderColor != null
-                      ? Border.all(
-                        color: borderColor,
-                        width: tokens.borderWidth150,
-                      )
-                      : null,
-              borderRadius: BorderRadius.all(tokens.borderRadius100),
-            ),
-            duration: buttonAnimationDuration,
-            child: IconTheme.merge(
-              data: IconThemeData(
-                color: _variant.getForegroundColor(
+          onTapHint: widget.semanticOnTapHint,
+          child: GestureWrapper(
+            onHoverChanged: _handleHoverChanged,
+            onPressedChanged: _handlePressedChanged,
+            onTap: widget.onPressed,
+            child: AnimatedContainer(
+              height: context.getContainerSize(widget.size),
+              width: context.getContainerSize(widget.size),
+              padding: EdgeInsets.zero,
+              decoration: BoxDecoration(
+                color: _variant.getBackgroundColor(
                   tokens,
                   isEnabled: _isEnabled,
                   isPressed: _isPressed,
                   isHovered: _isHovered,
                 ),
-                size: context.getIconSize(widget.size),
+                border:
+                    borderColor != null
+                        ? Border.all(
+                          color: borderColor,
+                          width: tokens.borderWidth150,
+                        )
+                        : null,
+                borderRadius: BorderRadius.all(tokens.borderRadius100),
               ),
-              child: widget.icon,
+              duration: buttonAnimationDuration,
+              child: IconTheme.merge(
+                data: IconThemeData(
+                  color: _variant.getForegroundColor(
+                    tokens,
+                    isEnabled: _isEnabled,
+                    isPressed: _isPressed,
+                    isHovered: _isHovered,
+                  ),
+                  size: context.getIconSize(widget.size),
+                ),
+                child: widget.icon,
+              ),
             ),
           ),
         ),
