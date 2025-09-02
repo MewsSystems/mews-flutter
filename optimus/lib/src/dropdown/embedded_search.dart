27b17835e07dd698d0747092aaa4e022a8bfe47e
Vitalij Vaščenko <vitalij.vascenko@mews.com>
2025-09-02 18:04:10 +0200

    linter

--- optimus/lib/src/dropdown/embedded_search.dart
+++ optimus/lib/src/dropdown/embedded_search.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/semantics.dart';
 import 'package:optimus/optimus.dart';
 
 /// An embedded search that is designed to be used inside the dropdown menu.
